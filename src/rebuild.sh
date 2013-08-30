85bda5b861e1d53770af3c5307d950ace7383bde
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2013-08-29 18:15:59 -0700

    The .rc file suffix was deprecated

--- src/rebuild.sh
+++ src/rebuild.sh
@@ -2,8 +2,8 @@
 
 ./clean.sh
 
-( cd lib && rustc *.rc ) || false
-rustc ./protobuf-bin-gen-rust.rc
-rustc ./protoc-gen-rust.rc
+rustc lib/protobuf.rs
+rustc ./protobuf-bin-gen-rust.rs
+rustc ./protoc-gen-rust.rs
 
 # vim: set ts=4 sw=4 et:
