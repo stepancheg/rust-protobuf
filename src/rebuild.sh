fb18522ebc2c415e22daec80fb589882ceb7d0f5
Hans Jørgen Hoel <hansjorg@gmail.com>
2013-09-20 15:28:01 +0200

    Add -L lib to rustc command line

--- src/rebuild.sh
+++ src/rebuild.sh
@@ -3,7 +3,7 @@
 ./clean.sh
 
 rustc lib/protobuf.rs
-rustc ./protobuf-bin-gen-rust.rs
-rustc ./protoc-gen-rust.rs
+rustc -L lib ./protobuf-bin-gen-rust.rs
+rustc -L lib ./protoc-gen-rust.rs
 
 # vim: set ts=4 sw=4 et:
