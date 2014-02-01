af4692e6cf79b4cd472038f352bbe834e1cbad01
Stepan Koltsov <stepan.koltsov@gmail.com>
2014-02-01 22:18:31 +0000

    run tests during full rebuild

--- src/clean.sh
+++ src/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -ex
 
 find . -name '*.dylib' -o -name '*.dSYM' -o -name '*.bin' -o -name '*.so' | xargs rm -rf
+rm -f lib/protobuf
 
 # vim: set ts=4 sw=4 et:
--- src/full-rebuild.sh
+++ src/full-rebuild.sh
@@ -4,5 +4,6 @@
 ./regenerate.sh
 ./rebuild.sh
 rustc --test lib/protobuf.rs
+./lib/protobuf
 
 # vim: set ts=4 sw=4 et:
