39b9b862e89a27599e39b3a4a429cbf714c78af0
Stepan Koltsov <stepan.koltsov@gmail.com>
2014-04-06 11:31:44 +0000

    Clean *.rlib too

--- src/clean.sh
+++ src/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -ex
 
-find . -name '*.dylib' -o -name '*.dSYM' -o -name '*.bin' -o -name '*.so' | xargs rm -rf
+find . -name '*.dylib' -o -name '*.rlib' -o -name '*.dSYM' -o -name '*.bin' -o -name '*.so' | xargs rm -rf
 rm -f lib/protobuf
 
 # vim: set ts=4 sw=4 et:
