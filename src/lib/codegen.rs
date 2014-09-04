a274f5ef33c60df6f349bb14b7e2d09aa6b02b4b
Jan Segre <jan@segre.in>
2014-09-04 17:28:00 -0300

    Disable warning about the mod name.

--- src/lib/codegen.rs
+++ src/lib/codegen.rs
@@ -1543,6 +1543,7 @@ pub fn gen(files: &[FileDescriptorProto], _: &GenOptions) -> Vec<GenResult> {
             w.write_line("");
             w.write_line("#![allow(dead_code)]");
             w.write_line("#![allow(non_camel_case_types)]");
+            w.write_line("#![allow(non_snake_case)]");
             w.write_line("#![allow(unused_imports)]");
 
             w.write_line("");
