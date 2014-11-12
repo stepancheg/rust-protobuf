eb9b55a096555179ffa1b51a0d0d5f6eb55b07aa
Stepan Koltsov <stepan.koltsov@gmail.com>
2014-11-13 02:09:19 +0300

    use cargo for rebuild

--- src/build-test.sh
+++ src/build-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
-where_am_i=$(cd `dirname $0`; pwd)
-PATH="$where_am_i:$PATH"
+where_am_i=$(cd `dirname $0`/..; pwd)
+PATH="$where_am_i/target:$PATH"
 
 protoc --rust_out lib proto/shrug.proto
 protoc --rust_out lib proto/test_root.proto
@@ -9,4 +9,5 @@ protoc --rust_out lib proto/test_nonunique_enum.proto
 protoc --rust_out lib proto/test_ident.proto
 protoc --rust_out lib proto/test_lite_runtime.proto
 protoc --rust_out lib proto/text_format_test_data.proto
-rustc -g --test lib/protobuf.rs -o test
+
+cargo test
--- src/rebuild.sh
+++ src/rebuild.sh
@@ -2,10 +2,6 @@
 
 ./clean.sh
 
-OPT=${RUST_PROTOBUF_OPT:--O}
-
-rustc $OPT -g lib/protobuf.rs
-rustc $OPT -g -L . ./protobuf-bin-gen-rust.rs
-rustc $OPT -g -L . ./protoc-gen-rust.rs
+cargo build
 
 # vim: set ts=4 sw=4 et:
