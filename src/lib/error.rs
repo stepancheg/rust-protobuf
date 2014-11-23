f69f3acde21c58c65242afb2c1a9f87c20a05524
Stepan Koltsov <stepan.koltsov@gmail.com>
2014-11-23 18:56:20 +0300

    Implement Error for ProtobufError
    Fixes #52

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -1,4 +1,5 @@
 use std::io::IoError;
+use std::error::Error;
 
 pub type ProtobufResult<T> = Result<T, ProtobufError>;
 
@@ -6,4 +7,21 @@ pub type ProtobufResult<T> = Result<T, ProtobufError>;
 pub enum ProtobufError {
     IoError(IoError),
     WireError(String),
+}
+
+impl Error for ProtobufError {
+    fn description(&self) -> &str {
+        match self {
+            // not sure that cause should be included in message
+            &ProtobufError::IoError(ref e) => e.description(),
+            &ProtobufError::WireError(ref e) => e.as_slice(),
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match self {
+            &ProtobufError::IoError(ref e) => Some(e as &Error),
+            &ProtobufError::WireError(..) => None,
+        }
+    }
 }
