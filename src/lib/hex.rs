3dcd02795922cb85d0c97b6211a71ee968401a95
Stepan Koltsov <stepan.koltsov@gmail.com>
2014-11-25 03:55:58 +0300

    Ascii to_byte renamed to as_byte

--- src/lib/hex.rs
+++ src/lib/hex.rs
@@ -17,7 +17,7 @@ pub fn decode_hex(hex: &str) -> Vec<u8> {
     let mut r: Vec<u8> = Vec::new();
     let mut pos = 0;
     loop {
-        while pos < hex.char_len() && hex.to_ascii().get(pos).unwrap().to_byte() == b' ' {
+        while pos < hex.char_len() && hex.to_ascii().get(pos).unwrap().as_byte() == b' ' {
             pos += 1;
         }
         if hex.char_len() - pos >= 2 {
