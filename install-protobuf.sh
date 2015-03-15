cabca6d5bf2d8129f26f957c53ab8a34939c5692
Stepan Koltsov <stepan.koltsov@gmail.com>
2015-03-15 04:16:06 +0300

    enable test with protobuf 3

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env sh
 
+die() {
+    echo "$@" >&2
+    exit 1
+}
+
 set -ex
 
-version=2.6.1
-basename=protobuf-$version
+test -n "$PROTOBUF_VERSION" || die "PROTOBUF_VERSION env var is undefined"
 
-curl -sL https://github.com/google/protobuf/releases/download/v$version/$basename.tar.bz2 | tar jx
+case "$PROTOBUF_VERSION" in
+2*)
+    basename=protobuf-$PROTOBUF_VERSION
+    ;;
+3*)
+    basename=protobuf-cpp-$PROTOBUF_VERSION
+    ;;
+*)
+    die "unknown protobuf version: $PROTOBUF_VERSION"
+    ;;
+esac
 
-cd $basename && ./configure --prefix=/usr && make && make install
+curl -sL https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz | tar zx
+
+cd protobuf-$PROTOBUF_VERSION
+
+./configure --prefix=/usr && make && make install
