1fd466f5fade41706e6bfbcae4ed735d3cb0e309
Stepan Koltsov <stepan.koltsov@gmail.com>
2015-03-21 05:43:42 +0300

    follow rust master: fix compilation

--- src/test/test_oneof.rs
+++ src/test/test_oneof.rs
@@ -5,8 +5,8 @@ use test::*;
 use test_oneof_proto::*;
 
 #[test]
-fn test1() {
-    let mut test1 = TestOneof::new();
-    test1.set_uint32_field(150);
-    test_serialize_deserialize("28 96 01", &test1);
+fn test_simple() {
+    let mut test_message = TestOneof::new();
+    test_message.set_uint32_field(150);
+    test_serialize_deserialize("28 96 01", &test_message);
 }
