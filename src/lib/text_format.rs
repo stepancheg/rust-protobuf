1d46560dfa6ffefaa0991768da99fcf0abce00f4
Jay Lee <busyjaylee@gmail.com>
2016-06-02 03:04:12 +0300

    print: keep interoperability with other language implementation

--- src/lib/text_format.rs
+++ src/lib/text_format.rs
@@ -7,18 +7,20 @@ use descriptor::*;
 
 fn print_bytes_to(bytes: &[u8], buf: &mut String) {
     buf.push('"');
-    for &b in bytes.iter() {
-        if b < 0x20 || b >= 0x7f {
-            buf.push('\\');
-            buf.push((b'0' + ((b >> 6) & 3)) as char);
-            buf.push((b'0' + ((b >> 3) & 7)) as char);
-            buf.push((b'0' + (b & 7)) as char);
-        } else if b == b'"' {
-            buf.push_str("\\\"");
-        } else if b == b'\\' {
-            buf.push_str("\\\\");
-        } else {
-            buf.push(b as char);
+    for &c in bytes {
+        match c {
+            b'\n' => buf.push_str(r"\n"),
+            b'\r' => buf.push_str(r"\r"),
+            b'\t' => buf.push_str(r"\t"),
+            b'"' => buf.push_str("\\\""),
+            b'\\' => buf.push_str(r"\\"),
+            b'\x20'...b'\x7e' => buf.push(c as char),
+            _ => {
+                buf.push('\\');
+                buf.push((b'0' + (c >> 6)) as char);
+                buf.push((b'0' + ((c >> 3) & 7)) as char);
+                buf.push((b'0' + (c & 7)) as char);
+            }
         }
     }
     buf.push('"');
@@ -245,4 +247,22 @@ pub fn print_to_string(m: &Message) -> String {
 pub fn fmt(m: &Message, f: &mut fmt::Formatter) -> fmt::Result {
     let pretty = f.alternate();
     f.write_str(&print_to_string_internal(m, pretty))
+}
+
+#[cfg(test)]
+mod test {
+
+    fn escape(data: &[u8]) -> String {
+        let mut s = String::with_capacity(data.len() * 4);
+        super::print_bytes_to(data, &mut s);
+        s
+    }
+
+    #[test]
+    fn test_print_to_bytes() {
+        assert_eq!("\"ab\"", escape(b"ab"));
+        assert_eq!("\"a\\\\023\"", escape(b"a\\023"));
+        assert_eq!("\"a\\r\\n\\t '\\\"\\\\\"", escape(b"a\r\n\t '\"\\"));
+        assert_eq!("\"\\344\\275\\240\\345\\245\\275\"", escape("你好".as_bytes()));
+    }
 }
