12edd11df61b87e498be6b96fee582a42268248c
Stepan Koltsov <stepan.koltsov@gmail.com>
2016-08-22 04:27:46 +0300

    move plugin.proto to appropriate location

--- src/regenerate.sh
+++ src/regenerate.sh
@@ -3,7 +3,7 @@
 where_am_i=$(cd `dirname $0`/..; pwd)
 PATH="$where_am_i/target/debug:$PATH"
 
-protoc --rust_out . -Iproto proto/google/protobuf/*.proto
+protoc --rust_out . -Iproto proto/google/protobuf/*.proto proto/google/protobuf/compiler/*
 
 mv descriptor.rs plugin.rs lib/
 
