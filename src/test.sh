501e5cd51bb48fac22112ca937cd42137dbc0333
Stepan Koltsov <stepan.koltsov@gmail.com>
2016-09-16 23:24:16 +0300

    move files

--- src/test.sh
+++ src/test.sh
@@ -5,14 +5,14 @@ cargo build
 where_am_i=$(cd `dirname $0`/..; pwd)
 PATH="$where_am_i/target/debug:$PATH"
 
-rm -f test/pb_*
+rm -f test2/pb_*
 
-protoc --rust_out test proto/pb_basic.proto
-protoc --rust_out test proto/pb_test-sanitize-file-name.proto
-protoc --rust_out test proto/pb_text_format_test_data.proto
-protoc --rust_out test -I proto proto/pb_test_*.proto
+protoc --rust_out test2 test2-proto/pb_basic.proto
+protoc --rust_out test2 test2-proto/pb_test-sanitize-file-name.proto
+protoc --rust_out test2 test2-proto/pb_text_format_test_data.proto
+protoc --rust_out test2 -I test2-proto test2-proto/pb_test_*.proto
 
-cd test
+cd test2
 
 rustc --test -L ../../target/debug lib.rs
 ./lib
