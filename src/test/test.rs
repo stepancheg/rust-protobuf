dbe6839197e527c46a6f63bcd6dc8dc90aad9b40
siddontang <siddontang@gmail.com>
2016-12-24 10:19:00 +0800

    foramt with rustfmt 0.6.0

--- src/test/test.rs
+++ src/test/test.rs
@@ -3,19 +3,19 @@ use protobuf::hex::decode_hex;
 
 use protobuf::*;
 
-pub fn test_serialize_deserialize_length_delimited<M : Message + MessageStatic>(msg: &M) {
+pub fn test_serialize_deserialize_length_delimited<M: Message + MessageStatic>(msg: &M) {
     let serialized_bytes = msg.write_length_delimited_to_bytes().unwrap();
     let parsed = parse_length_delimited_from_bytes::<M>(&serialized_bytes).unwrap();
     assert!(*msg == parsed);
 }
 
-pub fn test_serialize_deserialize_no_hex<M : Message + MessageStatic>(msg: &M) {
+pub fn test_serialize_deserialize_no_hex<M: Message + MessageStatic>(msg: &M) {
     let serialized_bytes = msg.write_to_bytes().unwrap();
     let parsed = parse_from_bytes::<M>(&serialized_bytes).unwrap();
     assert!(*msg == parsed);
 }
 
-pub fn test_serialize_deserialize<M : Message + MessageStatic>(hex: &str, msg: &M) {
+pub fn test_serialize_deserialize<M: Message + MessageStatic>(hex: &str, msg: &M) {
     let expected_bytes = decode_hex(hex);
     let expected_hex = encode_hex(&expected_bytes);
     let serialized = msg.write_to_bytes().unwrap();
@@ -29,13 +29,13 @@ pub fn test_serialize_deserialize<M : Message + MessageStatic>(hex: &str, msg: &
     test_serialize_deserialize_length_delimited(msg);
 }
 
-pub fn test_deserialize<M : Message + MessageStatic>(hex: &str, msg: &M) {
+pub fn test_deserialize<M: Message + MessageStatic>(hex: &str, msg: &M) {
     let bytes = decode_hex(hex);
     let parsed = parse_from_bytes::<M>(&bytes).unwrap();
     assert!(*msg == parsed);
 }
 
-pub fn test_serialize<M : Message + MessageStatic>(hex: &str, msg: &M) {
+pub fn test_serialize<M: Message + MessageStatic>(hex: &str, msg: &M) {
     let hex = encode_hex(&decode_hex(hex));
 
     let serialized = msg.write_to_bytes().unwrap();
