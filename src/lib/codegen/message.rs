1b1334d05e041d16902b5db98ca1e64c5e62134e
Dan Burkert <dan@danburkert.com>
2017-01-21 18:39:55 -0800

    Add Message::{as_any_mut, into_any}
    These methods are useful when working with abstract Message trait objects.

--- src/lib/codegen/message.rs
+++ src/lib/codegen/message.rs
@@ -2176,6 +2176,12 @@ impl<'a> MessageGen<'a> {
             w.def_fn("as_any(&self) -> &::std::any::Any", |w| {
                 w.write_line("self as &::std::any::Any");
             });
+            w.def_fn("as_any_mut(&mut self) -> &mut ::std::any::Any", |w| {
+                w.write_line("self as &mut ::std::any::Any");
+            });
+            w.def_fn("into_any(self: Box<Self>) -> ::std::boxed::Box<::std::any::Any>", |w| {
+                w.write_line("self");
+            });
             w.write_line("");
             w.def_fn("descriptor(&self) -> &'static ::protobuf::reflect::MessageDescriptor", |w| {
                 w.write_line("::protobuf::MessageStatic::descriptor_static(None::<Self>)");
