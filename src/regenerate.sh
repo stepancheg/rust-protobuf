3bd9b06fa00eadc595c87bb7793b6d895cb5c5c1
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-04-23 05:07:05 +0300

    Do not write temporary files to current directory

--- src/regenerate.sh
+++ src/regenerate.sh
@@ -16,11 +16,14 @@ esac
 where_am_i=$(cd `dirname $0`/..; pwd)
 PATH="$where_am_i/target/debug:$PATH"
 
-protoc --rust_out . -Iproto \
+rm -rf tmp-generated
+mkdir tmp-generated
+
+protoc --rust_out tmp-generated -Iproto \
     proto/google/protobuf/*.proto \
     proto/google/protobuf/compiler/* \
     proto/rustproto.proto
 
-mv descriptor.rs plugin.rs rustproto.rs lib/
+mv tmp-generated/descriptor.rs tmp-generated/plugin.rs tmp-generated/rustproto.rs lib/
 
 # vim: set ts=4 sw=4 et:
