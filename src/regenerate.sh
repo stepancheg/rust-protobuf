eb5b9dc35589aa8936cf424abfb07b4c07175741
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-04-23 04:58:12 +0300

    Only regenerate with proto 3

--- src/regenerate.sh
+++ src/regenerate.sh
@@ -1,5 +1,18 @@
 #!/bin/sh -ex
 
+die() {
+    echo "$@" >&2
+    exit 1
+}
+
+protoc_ver=$(protoc --version)
+case "$protoc_ver" in
+    "libprotoc 3"*) ;;
+    *)
+        die "you need to use protobuf 3 to regenerate .rs from .proto"
+    ;;
+esac
+
 where_am_i=$(cd `dirname $0`/..; pwd)
 PATH="$where_am_i/target/debug:$PATH"
 
