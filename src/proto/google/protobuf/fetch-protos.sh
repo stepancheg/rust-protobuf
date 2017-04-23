5132ab226981b58be4f4770cc369de7b58ebba1f
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-04-23 03:27:06 +0300

    use .proto files from protobuf 3.1.0

--- src/proto/google/protobuf/fetch-protos.sh
+++ src/proto/google/protobuf/fetch-protos.sh
@@ -4,7 +4,7 @@
 # comment out `reserved` directive for comatibility with protobuf 2.6.1
 # https://github.com/google/protobuf/issues/1669#issuecomment-240598089
 for f in descriptor.proto compiler/plugin.proto; do
-    curl -s https://raw.githubusercontent.com/google/protobuf/v3.0.0/src/google/protobuf/$f \
+    curl -s https://raw.githubusercontent.com/google/protobuf/v3.1.0/src/google/protobuf/$f \
         | sed -e 's,^\( *\)\(reserved.*\),\1// \2,' \
         > $f
 done
