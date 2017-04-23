992d931e3872da6f541fad9c5a599c066e4a8807
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-04-23 05:04:30 +0300

    Do not regenerate with proto 2

--- src/full-rebuild.sh
+++ src/full-rebuild.sh
@@ -1,13 +1,30 @@
 #!/bin/sh -ex
 
+die() {
+    echo "$@" >&2
+    exit 1
+}
+
 cd $(dirname $0)
 
 # Build protoc-gen-rust
 ./rebuild.sh
-# Generate from descriptor.proto
-./regenerate.sh
-# Build again with regenerated descriptor.proto
-./rebuild.sh
+
+protoc_ver=$(protoc --version)
+case "$protoc_ver" in
+    "libprotoc 3"*)
+        # Generate from descriptor.proto
+        ./regenerate.sh
+        # Build again with regenerated descriptor.proto
+        ./rebuild.sh
+        ;;
+    "libprotoc 2"*)
+		echo "do not regenerate with proto 2"
+        ;;
+    *)
+        die "unknown protoc version"
+        ;;
+esac
 
 ./test.sh
 
