e199a864c15abbe10715023e62be6eaa8a1e2886
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-06-09 03:43:38 +0300

    Fix compilation

--- protobuf-test/src/v3/test_zeros_are_not_written.rs
+++ protobuf-test/src/v3/test_zeros_are_not_written.rs
@@ -6,7 +6,7 @@ use super::test_zeros_are_not_written_pb::*;
 fn test_zeros_are_not_written() {
     let mut m = TestZerosAreNotWritten::new();
     m.set_bool_field(false);
-    m.set_enum_field(TestEnumDescriptor::UNKNOWN);
+    m.set_enum_field(TestEnumDescriptor::UNDEFINED);
     m.set_fixed32_field(0);
     test_serialize("", &m);
 }
