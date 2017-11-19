7a03aee4e67bdd25ae6c403f37386707a0ab5eb9
Stepan Koltsov <stepan.koltsov@gmail.com>
2017-11-19 04:36:40 +0000

    Drop old reflection accessors

--- protobuf/src/reflect/mod.rs
+++ protobuf/src/reflect/mod.rs
@@ -11,7 +11,6 @@ use descriptor::FieldDescriptorProto;
 use descriptor::EnumDescriptorProto;
 use descriptor::EnumValueDescriptorProto;
 use descriptor::FieldDescriptorProto_Label;
-use descriptor::FieldDescriptorProto_Type;
 use descriptorx::find_enum_by_rust_name;
 use descriptorx::find_message_by_rust_name;
 use reflect::accessor::FieldAccessor;
@@ -24,7 +23,6 @@ mod value;
 mod optional;
 
 use self::repeated::ReflectRepeated;
-use self::repeated::ReflectRepeatedRef;
 use self::map::ReflectMap;
 
 pub use self::value::ProtobufValue;
@@ -72,145 +70,48 @@ impl FieldDescriptor {
         self.accessor.get_message_generic(m)
     }
 
-    pub fn get_rep_message_item<'a>(&self, m: &'a Message, index: usize) -> &'a Message {
-        self.accessor.get_rep_message_item_generic(m, index)
-    }
-
     pub fn get_enum(&self, m: &Message) -> &'static EnumValueDescriptor {
         self.accessor.get_enum_generic(m)
-    }
-
-    pub fn get_rep_enum_item(&self, m: &Message, index: usize) -> &'static EnumValueDescriptor {
-        self.accessor.get_rep_enum_item_generic(m, index)
     }
 
     pub fn get_str<'a>(&self, m: &'a Message) -> &'a str {
         self.accessor.get_str_generic(m)
     }
 
-    pub fn get_rep_str<'a>(&self, m: &'a Message) -> &'a [String] {
-        self.accessor.get_rep_str_generic(m)
-    }
-
-    pub fn get_rep_str_item<'a>(&self, m: &'a Message, index: usize) -> &'a str {
-        &self.get_rep_str(m)[index]
-    }
-
     pub fn get_bytes<'a>(&self, m: &'a Message) -> &'a [u8] {
         self.accessor.get_bytes_generic(m)
-    }
-
-    pub fn get_rep_bytes<'a>(&self, m: &'a Message) -> &'a [Vec<u8>] {
-        self.accessor.get_rep_bytes_generic(m)
-    }
-
-    pub fn get_rep_bytes_item<'a>(&self, m: &'a Message, index: usize) -> &'a [u8] {
-        &self.get_rep_bytes(m)[index]
     }
 
     pub fn get_u32(&self, m: &Message) -> u32 {
         self.accessor.get_u32_generic(m)
     }
 
-    pub fn get_rep_u32<'a>(&self, m: &'a Message) -> &'a [u32] {
-        self.accessor.get_rep_u32_generic(m)
-    }
-
     pub fn get_u64(&self, m: &Message) -> u64 {
         self.accessor.get_u64_generic(m)
-    }
-
-    pub fn get_rep_u64<'a>(&self, m: &'a Message) -> &'a [u64] {
-        self.accessor.get_rep_u64_generic(m)
     }
 
     pub fn get_i32(&self, m: &Message) -> i32 {
         self.accessor.get_i32_generic(m)
     }
 
-    pub fn get_rep_i32<'a>(&self, m: &'a Message) -> &'a [i32] {
-        self.accessor.get_rep_i32_generic(m)
-    }
-
     pub fn get_i64(&self, m: &Message) -> i64 {
         self.accessor.get_i64_generic(m)
-    }
-
-    pub fn get_rep_i64<'a>(&self, m: &'a Message) -> &'a [i64] {
-        self.accessor.get_rep_i64_generic(m)
     }
 
     pub fn get_bool(&self, m: &Message) -> bool {
         self.accessor.get_bool_generic(m)
     }
 
-    pub fn get_rep_bool<'a>(&self, m: &'a Message) -> &'a [bool] {
-        self.accessor.get_rep_bool_generic(m)
-    }
-
     pub fn get_f32(&self, m: &Message) -> f32 {
         self.accessor.get_f32_generic(m)
-    }
-
-    pub fn get_rep_f32<'a>(&self, m: &'a Message) -> &'a [f32] {
-        self.accessor.get_rep_f32_generic(m)
     }
 
     pub fn get_f64(&self, m: &Message) -> f64 {
         self.accessor.get_f64_generic(m)
     }
 
-    pub fn get_rep_f64<'a>(&self, m: &'a Message) -> &'a [f64] {
-        self.accessor.get_rep_f64_generic(m)
-    }
-
     pub fn get_reflect<'a>(&self, m: &'a Message) -> ReflectFieldRef<'a> {
         self.accessor.get_reflect(m)
-    }
-
-    pub fn get_rep_item<'a>(&self, m: &'a Message, index: usize) -> ProtobufValueRef<'a> {
-        match self.proto().get_field_type() {
-            FieldDescriptorProto_Type::TYPE_MESSAGE => ProtobufValueRef::Message(
-                self.get_rep_message_item(m, index),
-            ),
-            FieldDescriptorProto_Type::TYPE_ENUM => ProtobufValueRef::Enum(
-                self.get_rep_enum_item(m, index),
-            ),
-            FieldDescriptorProto_Type::TYPE_STRING => ProtobufValueRef::String(
-                self.get_rep_str_item(m, index),
-            ),
-            FieldDescriptorProto_Type::TYPE_BYTES => ProtobufValueRef::Bytes(
-                self.get_rep_bytes_item(m, index),
-            ),
-            FieldDescriptorProto_Type::TYPE_INT32 |
-            FieldDescriptorProto_Type::TYPE_SINT32 |
-            FieldDescriptorProto_Type::TYPE_SFIXED32 => ProtobufValueRef::I32(
-                self.get_rep_i32(m)[index],
-            ),
-            FieldDescriptorProto_Type::TYPE_INT64 |
-            FieldDescriptorProto_Type::TYPE_SINT64 |
-            FieldDescriptorProto_Type::TYPE_SFIXED64 => ProtobufValueRef::I64(
-                self.get_rep_i64(m)[index],
-            ),
-            FieldDescriptorProto_Type::TYPE_UINT32 |
-            FieldDescriptorProto_Type::TYPE_FIXED32 => ProtobufValueRef::U32(
-                self.get_rep_u32(m)[index],
-            ),
-            FieldDescriptorProto_Type::TYPE_UINT64 |
-            FieldDescriptorProto_Type::TYPE_FIXED64 => ProtobufValueRef::U64(
-                self.get_rep_u64(m)[index],
-            ),
-            FieldDescriptorProto_Type::TYPE_BOOL => ProtobufValueRef::Bool(
-                self.get_rep_bool(m)[index],
-            ),
-            FieldDescriptorProto_Type::TYPE_FLOAT => ProtobufValueRef::F32(
-                self.get_rep_f32(m)[index],
-            ),
-            FieldDescriptorProto_Type::TYPE_DOUBLE => ProtobufValueRef::F64(
-                self.get_rep_f64(m)[index],
-            ),
-            FieldDescriptorProto_Type::TYPE_GROUP => unimplemented!(),
-        }
     }
 }
 
@@ -394,7 +295,6 @@ impl EnumDescriptor {
 
 pub enum ReflectFieldRef<'a> {
     Repeated(&'a ReflectRepeated),
-    RepeatedOld(ReflectRepeatedRef<'a>),
     Map(&'a ReflectMap),
     Optional(Option<ProtobufValueRef<'a>>),
 }
