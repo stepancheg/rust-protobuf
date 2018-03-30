399ef955f74fbbf716b2fe720b2e9f796151b7ba
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-03-30 23:39:16 +0100

    GC

--- protobuf-codegen-pure-test/src/v2/mod.rs
+++ protobuf-codegen-pure-test/src/v2/mod.rs
@@ -3,6 +3,3 @@ mod test_sanitize_file_name_pb;
 
 mod test_default_values;
 mod test_default_values_pb;
-
-mod test_first;
-mod test_first_pb;
--- protobuf-codegen-pure-test/src/v2/test_first.rs
+++
@@ -1,9 +0,0 @@
-use protobuf_test_common::*;
-use super::test_first_pb::*;
-
-#[test]
-fn test1() {
-    let mut test1 = Test1::new();
-    test1.set_a(150);
-    test_serialize_deserialize("08 96 01", &test1);
-}
