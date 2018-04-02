ca6dc4fe9fe8349b5cc4d4bba426e12f033772be
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-04-02 15:22:35 +0100

    Fix unused imports warning

--- protobuf-test/src/v2/test_unknown_suffix.rs
+++ protobuf-test/src/v2/test_unknown_suffix.rs
@@ -1,1 +1,6 @@
-use super::test_unknown_suffix_pb_proto3::TestUnknownSuffix as _Ignore;
+use super::test_unknown_suffix_pb_proto3::TestUnknownSuffix;
+
+#[test]
+fn test() {
+    TestUnknownSuffix::new();
+}
