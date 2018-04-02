079104cdf83773a0c5c280f93273e31712e840ee
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-04-02 10:52:12 +0100

    Use assert_eq! macro in tests
    This makes test failure output easier to read

--- protobuf-test-common/src/test.rs
+++ protobuf-test-common/src/test.rs
@@ -6,13 +6,13 @@ use protobuf::*;
 pub fn test_serialize_deserialize_length_delimited<M : Message + PartialEq>(msg: &M) {
     let serialized_bytes = msg.write_length_delimited_to_bytes().unwrap();
     let parsed = parse_length_delimited_from_bytes::<M>(&serialized_bytes).unwrap();
-    assert!(*msg == parsed);
+    assert_eq!(*msg, parsed);
 }
 
 pub fn test_serialize_deserialize_no_hex<M : Message + PartialEq>(msg: &M) {
     let serialized_bytes = msg.write_to_bytes().unwrap();
     let parsed = parse_from_bytes::<M>(&serialized_bytes).unwrap();
-    assert!(*msg == parsed);
+    assert_eq!(*msg, parsed);
 }
 
 pub fn test_serialize_deserialize<M : Message + PartialEq>(hex: &str, msg: &M) {
@@ -27,7 +27,7 @@ pub fn test_serialize_deserialize<M : Message + PartialEq>(hex: &str, msg: &M) {
         M::descriptor_static(None).name()
     );
     let parsed = parse_from_bytes::<M>(&expected_bytes).unwrap();
-    assert!(*msg == parsed);
+    assert_eq!(*msg, parsed);
 
     assert_eq!(expected_bytes.len(), msg.compute_size() as usize);
 
@@ -37,7 +37,7 @@ pub fn test_serialize_deserialize<M : Message + PartialEq>(hex: &str, msg: &M) {
 pub fn test_deserialize<M : Message + PartialEq>(hex: &str, msg: &M) {
     let bytes = decode_hex(hex);
     let parsed = parse_from_bytes::<M>(&bytes).unwrap();
-    assert!(*msg == parsed);
+    assert_eq!(*msg, parsed);
 }
 
 pub fn test_serialize<M : Message>(hex: &str, msg: &M) {
