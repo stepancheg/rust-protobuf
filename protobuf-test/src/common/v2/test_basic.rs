6dd5dbde730ccd6fb1b4dafdef5a2e2f7d4834c4
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-04-02 09:58:30 +0100

    Extract repeated packed tests into separate files

--- protobuf-test/src/common/v2/test_basic.rs
+++ protobuf-test/src/common/v2/test_basic.rs
@@ -37,22 +37,6 @@ fn test4() {
 }
 
 #[test]
-fn test_read_unpacked_expect_packed() {
-    let mut test_packed_unpacked = TestPackedUnpacked::new();
-    test_packed_unpacked.set_packed(Vec::new());
-    test_packed_unpacked.set_unpacked([17i32, 1000].to_vec());
-    test_deserialize("20 11 20 e8 07", &test_packed_unpacked);
-}
-
-#[test]
-fn test_read_packed_expect_unpacked() {
-    let mut test_packed_unpacked = TestPackedUnpacked::new();
-    test_packed_unpacked.set_packed([17i32, 1000].to_vec());
-    test_packed_unpacked.set_unpacked(Vec::new());
-    test_deserialize("2a 03 11 e8 07", &test_packed_unpacked);
-}
-
-#[test]
 fn test_end_by_negative_int() {
     // added following https://github.com/stepancheg/rust-protobuf/pull/209
     let mut test = Test1::new();
