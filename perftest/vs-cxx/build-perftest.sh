cb11b6df85d86b702247780a5be40d9a212d829d
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-05-28 22:51:36 +0100

    drop perftest
    It's hard to maintain it, and it's not needed in the stable branch

--- perftest/vs-cxx/build-perftest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-
-cd $(dirname $0)
-
-./build-perftest-data.sh
-cargo build --release
-./build-perftest-cxx.sh
-
-# vim: set ts=4 sw=4 et:
--- perftest/vs-cxx/build.rs
+++
@@ -1,9 +0,0 @@
-extern crate protoc_rust;
-
-fn main() {
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: ".",
-        input: &["perftest_data.proto"],
-        ..Default::default()
-    }).expect("protoc");
-}
