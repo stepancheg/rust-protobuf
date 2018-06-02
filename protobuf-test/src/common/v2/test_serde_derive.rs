1a575d2d069fc26dd93571374599b952cc949055
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-06-02 16:43:36 -0400

    Test serialize/deserializegs

--- protobuf-test/src/common/v2/test_serde_derive.rs
+++ protobuf-test/src/common/v2/test_serde_derive.rs
@@ -1,17 +1,15 @@
 use super::test_serde_derive_pb::*;
 use serde_json;
 
-#[derive(Serialize, Deserialize)]
-#[serde(deny_unknown_fields)]
-struct Proto(TestSerde);
-
 #[test]
-fn serialize () {
+fn serialize_deserialize () {
     let mut data = TestSerde::new();
     data.set_test(SerdeEnum::TEST);
 
     let serialized = serde_json::to_string(&data).unwrap();
 
     assert_eq!(serialized, "{\"test\":\"TEST\"}");
-    println!("{:#?}", serialized);
+
+    let deserialized: TestSerde = serde_json::from_str(&serialized).unwrap();
+    assert_eq!(deserialized, data);
 }
