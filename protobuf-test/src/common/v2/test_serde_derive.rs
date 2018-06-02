40f9fab8130dc7a66e84055ceb88fe53d2d3ebc7
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-06-02 17:00:00 -0400

    Test protobuf map

--- protobuf-test/src/common/v2/test_serde_derive.rs
+++ protobuf-test/src/common/v2/test_serde_derive.rs
@@ -1,15 +1,20 @@
 use super::test_serde_derive_pb::*;
 use serde_json;
+use std::collections::HashMap;
 
 #[test]
 fn serialize_deserialize () {
-    let mut data = TestSerde::new();
-    data.set_test(SerdeEnum::TEST);
+    let mut original_data = TestSerde::new();
+    original_data.set_test_enum(SerdeEnum::TEST);
 
-    let serialized = serde_json::to_string(&data).unwrap();
+    let mut map = HashMap::new();
+    map.insert(5, 10);
+    original_data.set_somemap(map);
 
-    assert_eq!(serialized, "{\"test\":\"TEST\"}");
+    let serialized = serde_json::to_string(&original_data).unwrap();
+
+    assert_eq!(serialized, r#"{"test_enum":"TEST","somemap":{"5":10}}"#);
 
     let deserialized: TestSerde = serde_json::from_str(&serialized).unwrap();
-    assert_eq!(deserialized, data);
+    assert_eq!(deserialized, original_data);
 }
