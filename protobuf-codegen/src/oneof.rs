b041fbe12ff08480d598bfbf54a6dc678aec8465
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-06-02 23:10:54 -0400

    Remove comment

--- protobuf-codegen/src/oneof.rs
+++ protobuf-codegen/src/oneof.rs
@@ -157,7 +157,6 @@ impl<'a> OneofGen<'a> {
             derive.push("Debug");
         }
         if self.customize.serde_derive.unwrap_or(false) {
-            // derive.extend(&["Serialize", "Deserialize"]);
             derive.extend(&["Serialize", "Deserialize"]);
         }
         w.derive(&derive);
