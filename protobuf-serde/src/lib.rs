52e41fbd1ec7e6f4bc8492cf58675c8f04343916
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-06-02 23:24:25 -0400

    Reformat protobuf serde

--- protobuf-serde/src/lib.rs
+++ protobuf-serde/src/lib.rs
@@ -10,13 +10,14 @@ use serde::ser::Serializer;
 use serde::Deserializer;
 
 pub fn serialize_singular_ptr_field<'a, T: Serialize, S>(spf: &SingularPtrField<T>, serializer: S)
-                                                         -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
-    where S: Serializer {
+        -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
+    where S: Serializer
+{
     spf.get_ref().serialize(serializer)
 }
 
 pub fn deserialize_singular_ptr_field<'de, D, T>(deserializer: D)
-                                                 -> Result<SingularPtrField<T>, D::Error>
+        -> Result<SingularPtrField<T>, D::Error>
     where D: Deserializer<'de>, T: Deserialize<'de>
 {
     Ok(SingularPtrField::some(Option::deserialize(deserializer).unwrap().unwrap()))
