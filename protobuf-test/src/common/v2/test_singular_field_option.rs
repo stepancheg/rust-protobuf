ed0bb6a92e4d5cf054f59128acbfb0958f5d6190
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-06-18 00:50:02 +0100

    More tests for singular_field options

--- protobuf-test/src/common/v2/test_singular_field_option.rs
+++ protobuf-test/src/common/v2/test_singular_field_option.rs
@@ -1,16 +1,20 @@
 use super::test_singular_field_option_pb::*;
+use protobuf::SingularPtrField;
 
 #[test]
 fn test_option_box() {
-    let _ = WithOptionBox::new();
+    let w = WithOptionBox::new();
+    let _inner: &Option<Box<Inner>> = &w.inner;
 }
 
 #[test]
 fn test_option() {
-    let _ = WithOption::new();
+    let w = WithOption::new();
+    let _inner: &Option<Inner> = &w.inner;
 }
 
 #[test]
-fn test_singular_field() {
-    let _ = WithSingularField::new();
+fn test_singular_ptr_field() {
+    let w = WithSingularField::new();
+    let _inner: &SingularPtrField<Inner> = &w.inner;
 }
