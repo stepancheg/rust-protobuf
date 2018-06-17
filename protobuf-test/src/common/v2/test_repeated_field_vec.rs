1db004a7b653462575d48d8dcba0b92c343f2198
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-06-17 02:44:40 +0100

    Better test for repeated_field_vec option

--- protobuf-test/src/common/v2/test_repeated_field_vec.rs
+++ protobuf-test/src/common/v2/test_repeated_field_vec.rs
@@ -1,7 +1,14 @@
 use super::test_repeated_field_vec_pb::*;
+use protobuf::RepeatedField;
 
 #[test]
-fn test() {
-    let basket = Basket::new();
+fn test_vec() {
+    let basket = BasketVec::new();
     let _eggs: Vec<Egg> = basket.eggs;
+}
+
+#[test]
+fn test_repeated_field() {
+    let basket = BasketRepeatedField::new();
+    let _eggs: RepeatedField<Egg> = basket.eggs;
 }
