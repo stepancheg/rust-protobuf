98b30f0bc004b673dab9bf1711c999d93f47f577
Vasily Chekalkin <bacek@bacek.com>
2018-09-27 13:32:58 +1000

    Link with serde when with-serde feature is enabled

--- protobuf/src/lib.rs
+++ protobuf/src/lib.rs
@@ -7,7 +7,12 @@
 
 #[cfg(feature = "bytes")]
 extern crate bytes;
+#[cfg(feature = "with-serde")]
+extern crate serde;
+#[macro_use]
+#[cfg(feature = "with-serde")]
 
+extern crate serde_derive;
 pub use unknown::UnknownFields;
 pub use unknown::UnknownFieldsIter;
 pub use unknown::UnknownValue;
