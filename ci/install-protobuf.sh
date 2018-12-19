269f23559c0032aca68d6cd131d2e3e28245125f
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-12-19 07:56:54 +0000

    Download prebuilt protoc on Windows
    instead of full protobuf from sources

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -7,31 +7,44 @@ die() {
     exit 1
 }
 
-if [ `uname` = Linux ]; then
-    # Check we have ccache
-    ccache --version
-    export CC="ccache gcc"
-    export CXX="ccache g++"
-fi
-
 test -n "$PROTOBUF_VERSION" || die "PROTOBUF_VERSION env var is undefined"
 
-case "$PROTOBUF_VERSION" in
-2*)
-    basename=protobuf-$PROTOBUF_VERSION
+case `uname` in
+    Linux)
+        # Check we have ccache
+        ccache --version
+        export CC="ccache gcc"
+        export CXX="ccache g++"
+
+        case "$PROTOBUF_VERSION" in
+        2*)
+            basename=protobuf-$PROTOBUF_VERSION
+            ;;
+        3*)
+            basename=protobuf-cpp-$PROTOBUF_VERSION
+            ;;
+        *)
+            die "unknown protobuf version: $PROTOBUF_VERSION"
+            ;;
+        esac
+
+        curl -sL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz | tar zx
+
+        cd protobuf-$PROTOBUF_VERSION
+
+        ./configure --prefix=$HOME && make -j2 && make install
+
     ;;
-3*)
-    basename=protobuf-cpp-$PROTOBUF_VERSION
+    MSYS_NT*)
+        (
+            cd $HOME
+            curl -sLO https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/protoc-$PROTOBUF_VERSION-win32.zip
+            unzip protoc-$PROTOBUF_VERSION-win32.zip
+        )
     ;;
-*)
-    die "unknown protobuf version: $PROTOBUF_VERSION"
+    *)
+        die "unknown uname: `uname`"
     ;;
 esac
 
-curl -sL https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz | tar zx
-
-cd protobuf-$PROTOBUF_VERSION
-
-./configure --prefix=/home/travis && make -j2 && make install
-
-test -x /home/travis/bin/protoc
+$HOME/bin/protoc --version
