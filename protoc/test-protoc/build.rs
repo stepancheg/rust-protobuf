d3b88885cef16d2a0b1d7c110aa21448df69ee5a
Stepan Koltsov <stepan.koltsov@gmail.com>
2018-12-29 01:21:00 +0000

    Fix test-protoc on Windows

--- protoc/test-protoc/build.rs
+++ protoc/test-protoc/build.rs
@@ -1,10 +1,25 @@
 extern crate protoc;
 
+use std::path::PathBuf;
+
 fn main() {
+    let exe_suffix = if cfg!(windows) {
+        ".exe"
+    } else if cfg!(unix) {
+        ""
+    } else {
+        panic!("unknown OS")
+    };
+
+    let protoc_gen_rust = PathBuf::from(format!("../../target/debug/protoc-gen-rust{}", exe_suffix));
+    let protoc_gen_rust = protoc_gen_rust.canonicalize().expect("canonicalize");
+
+    assert!(protoc_gen_rust.is_file(), "{:?}", protoc_gen_rust);
+
     protoc::run(protoc::Args {
         lang: "rust",
         out_dir: "src",
-        plugin: Some("../../target/debug/protoc-gen-rust"),
+        plugin: Some(&format!("protoc-gen-rust={}", protoc_gen_rust.as_os_str().to_str().unwrap())),
         input: &["src/data.proto"],
         ..Default::default()
     }).expect("protoc");
