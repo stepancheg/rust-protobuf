ce66dbbce8b3001b8a980f2c4436226c684fd839
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-02-03 01:56:55 +0000

    Remove unused AsAny function
    Reduces protobuf_test size from 33385564 to 32612308 (2%)

--- protobuf/src/reflect/as_any.rs
+++ protobuf/src/reflect/as_any.rs
@@ -2,15 +2,13 @@
 //! https://github.com/rust-lang/rust/issues/5665#issuecomment-31582946
 
 use std::any::Any;
-use std::mem;
+
 
 /// Hack against lack of upcasting in Rust
 pub trait AsAny {
     fn as_any(&self) -> &Any;
     fn as_any_mut(&mut self) -> &mut Any;
     fn into_any_box(self: Box<Self>) -> Box<Any>;
-    fn set_from_any(&mut self, any: Box<Any>);
-    fn swap_with_any(&mut self, any: &mut Any);
 }
 
 impl<T: 'static> AsAny for T {
@@ -24,14 +22,5 @@ impl<T: 'static> AsAny for T {
 
     fn into_any_box(self: Box<Self>) -> Box<Any> {
         Box::new(*self)
-    }
-
-    fn set_from_any(&mut self, any: Box<Any>) {
-        *self = *any.downcast().expect("wrong type");
-    }
-
-    fn swap_with_any(&mut self, any: &mut Any) {
-        let that = any.downcast_mut().expect("wrong type");
-        mem::swap(self, that);
     }
 }
