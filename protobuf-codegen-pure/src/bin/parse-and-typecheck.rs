a8c66d612c1bb29bf425b616a178cd88b65c22b8
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-02-21 03:45:05 +0000

    Improve internal parse-and-typecheck utility
    * print multiline protos
    * support multiple include directories

--- protobuf-codegen-pure/src/bin/parse-and-typecheck.rs
+++ protobuf-codegen-pure/src/bin/parse-and-typecheck.rs
@@ -4,12 +4,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    assert_eq!(2, args.len());
-    let includes = vec![&args[0][..]];
-    let input = vec![&args[1][..]];
+    assert!(args.len() >= 2);
+    let input = vec![&args[0][..]];
+    let includes: Vec<&str> = args[1..].iter().map(|s| s.as_str()).collect();
     let t =
         protobuf_codegen_pure::parse_and_typecheck(&includes, &input).expect("parse_and_typecheck");
     for fd in t.file_descriptors {
-        println!("{:?}", fd);
+        println!("{:#?}", fd);
     }
 }
