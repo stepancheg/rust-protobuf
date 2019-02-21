134e87b61c50b54ffaa0efa427bb22d3c5f65ec1
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-02-21 04:29:05 +0000

    Remove unsafe code in encode_varint* implementations

--- protobuf/src/varint.rs
+++ protobuf/src/varint.rs
@@ -4,16 +4,31 @@
 pub fn encode_varint64(mut value: u64, buf: &mut [u8]) -> usize {
     assert!(buf.len() >= 10);
 
-    unsafe {
-        let mut i = 0;
-        while (value & !0x7F) > 0 {
-            *buf.get_unchecked_mut(i) = ((value & 0x7F) | 0x80) as u8;
-            value >>= 7;
-            i += 1;
+    fn iter(value: &mut u64, byte: &mut u8) -> bool {
+        if (*value & !0x7F) > 0 {
+            *byte = ((*value & 0x7F) | 0x80) as u8;
+            *value >>= 7;
+            true
+        } else {
+            *byte = *value as u8;
+            false
         }
-        *buf.get_unchecked_mut(i) = value as u8;
-        i + 1
     }
+
+    // Explicitly unroll loop to avoid either
+    // unsafe code or bound checking when writing to `buf`
+
+    if !iter(&mut value, &mut buf[0]) { return 1; };
+    if !iter(&mut value, &mut buf[1]) { return 2; };
+    if !iter(&mut value, &mut buf[2]) { return 3; };
+    if !iter(&mut value, &mut buf[3]) { return 4; };
+    if !iter(&mut value, &mut buf[4]) { return 5; };
+    if !iter(&mut value, &mut buf[5]) { return 6; };
+    if !iter(&mut value, &mut buf[6]) { return 7; };
+    if !iter(&mut value, &mut buf[7]) { return 8; };
+    if !iter(&mut value, &mut buf[8]) { return 9; };
+    buf[9] = value as u8;
+    10
 }
 
 /// Encode u32 value as varint.
@@ -22,14 +37,24 @@ pub fn encode_varint64(mut value: u64, buf: &mut [u8]) -> usize {
 pub fn encode_varint32(mut value: u32, buf: &mut [u8]) -> usize {
     assert!(buf.len() >= 5);
 
-    unsafe {
-        let mut i = 0;
-        while (value & !0x7F) > 0 {
-            *buf.get_unchecked_mut(i) = ((value & 0x7F) | 0x80) as u8;
-            value >>= 7;
-            i += 1;
+    fn iter(value: &mut u32, byte: &mut u8) -> bool {
+        if (*value & !0x7F) > 0 {
+            *byte = ((*value & 0x7F) | 0x80) as u8;
+            *value >>= 7;
+            true
+        } else {
+            *byte = *value as u8;
+            false
         }
-        *buf.get_unchecked_mut(i) = value as u8;
-        i + 1
     }
+
+    // Explicitly unroll loop to avoid either
+    // unsafe code or bound checking when writing to `buf`
+
+    if !iter(&mut value, &mut buf[0]) { return 1; };
+    if !iter(&mut value, &mut buf[1]) { return 2; };
+    if !iter(&mut value, &mut buf[2]) { return 3; };
+    if !iter(&mut value, &mut buf[3]) { return 4; };
+    buf[4] = value as u8;
+    5
 }
