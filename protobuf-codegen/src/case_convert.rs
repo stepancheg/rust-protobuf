148ebc75919391ffc4fd008b6b40b169063f7713
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-03-24 18:36:36 +0000

    snake_case function to be used in nested modules generation

--- protobuf-codegen/src/case_convert.rs
+++ protobuf-codegen/src/case_convert.rs
@@ -18,12 +18,35 @@ pub fn camel_case(input: &str) -> String {
     result
 }
 
+pub fn snake_case(input: &str) -> String {
+    let mut result = String::new();
+
+    let mut last_lower = false;
+
+    for c in input.chars() {
+        if c.is_ascii_uppercase() && last_lower {
+            result.push('_');
+        }
+        result.push(c.to_ascii_lowercase());
+        last_lower = c.is_lowercase();
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn test_camel_case() {
-        assert_eq!("FooBarBaz", camel_case("foo_barBaz"));
+        assert_eq!("FooBarBazQuxQUUX", camel_case("foo_barBaz_QuxQUUX"));
+        assert_eq!("FooBarBazQuxQUUX", camel_case("Foo_barBaz_QuxQUUX"));
+    }
+
+    #[test]
+    fn test_snake_case() {
+        assert_eq!("foo_bar_baz_qux_quux", snake_case("foo_barBaz_QuxQUUX"));
+        assert_eq!("foo_bar_baz_qux_quux", snake_case("Foo_barBaz_QuxQUUX"));
     }
 }
