d3d92e104d0bfe9e437df3a402f31218abfb1274
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-05-27 00:10:10 +0100

    UnknownFields rustdoc

--- protobuf/src/unknown.rs
+++ protobuf/src/unknown.rs
@@ -131,6 +131,14 @@ impl<'o> Iterator for UnknownValuesIter<'o> {
     }
 }
 
+/// Hold "unknown" fields in parsed message.
+///
+/// Field may be unknown if it they are added in newer version of `.proto`.
+/// Unknown fields are stored in `UnknownFields` structure, so
+/// protobuf message could process messages without losing data.
+///
+/// For example, in this operation: load from DB, modify, store to DB,
+/// even when working with older `.proto` file, new fields won't be lost.
 #[derive(Clone, PartialEq, Eq, Debug, Default)]
 pub struct UnknownFields {
     // option is needed, because HashMap constructor performs allocation,
