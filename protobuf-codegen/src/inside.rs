14b5a837bf4a9e3dc7e40d14dd04798c40175b11
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-05-27 05:58:47 +0100

    Fix compilation on older rust
    Cannot use crate:: paths before Rust 1.32.0

--- protobuf-codegen/src/inside.rs
+++ protobuf-codegen/src/inside.rs
@@ -4,7 +4,8 @@ use Customize;
 /// used inside or outside of protobuf crate.
 pub(crate) fn protobuf_crate_path(customize: &Customize) -> &str {
     match customize.inside_protobuf {
-        Some(true) => "crate",
+        // Can't use `crate::` paths before Rust 1.32.0
+        //Some(true) => "crate",
         _ => "::protobuf",
     }
 }
