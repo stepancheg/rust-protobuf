faaa6fb8fa9623af115233da58a5cc212a839a1e
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-06-03 07:07:15 +0100

    Better reflection test
    Test that `Self` message name is actually `Self`

--- protobuf-test/src/common/v2/test_ident.rs
+++ protobuf-test/src/common/v2/test_ident.rs
@@ -1,6 +1,14 @@
 use super::test_ident_pb::*;
+use protobuf::Message;
 
 #[test]
 fn test() {
     let _ = TestType::new();
+}
+
+#[test]
+fn test_reflect() {
+    message_Self::new();
+    // instantiate reflection
+    assert_eq!("Self", message_Self::descriptor_static().name());
 }
