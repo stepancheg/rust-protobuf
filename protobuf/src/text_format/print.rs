8c53aa41dac7d1316b0ba37d3edd38bda9ed5801
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-06-03 04:02:03 +0100

    Replace deprecated ... syntax with ..=

--- protobuf/src/text_format/print.rs
+++ protobuf/src/text_format/print.rs
@@ -13,7 +13,7 @@ fn quote_bytes_to(bytes: &[u8], buf: &mut String) {
             b'\t' => buf.push_str(r"\t"),
             b'"' => buf.push_str("\\\""),
             b'\\' => buf.push_str(r"\\"),
-            b'\x20'...b'\x7e' => buf.push(c as char),
+            b'\x20'..=b'\x7e' => buf.push(c as char),
             _ => {
                 buf.push('\\');
                 buf.push((b'0' + (c >> 6)) as char);
