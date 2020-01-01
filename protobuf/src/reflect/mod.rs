d4a99a72d43223a193a506ce8a0cca2b1ff4bfd7
Stepan Koltsov <stepan.koltsov@gmail.com>
2020-01-01 01:27:17 +0000

    Fix unused imports

--- protobuf/src/reflect/mod.rs
+++ protobuf/src/reflect/mod.rs
@@ -1,14 +1,6 @@
 //! Reflection implementation for protobuf types.
 
-use std::collections::HashMap;
-use std::default::Default;
-use std::marker;
-
 use core::Message;
-use descriptor::DescriptorProto;
-use descriptor::FileDescriptorProto;
-use descriptorx::find_message_by_rust_name;
-use reflect::accessor::FieldAccessor;
 
 pub mod accessor;
 mod enums;
