3f15af8df40e9a75a69145d30c831216556482ae
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-04-05 22:35:20 +0100

    Print which version we are using

--- protoc/update-bin.sh
+++ protoc/update-bin.sh
@@ -4,8 +4,10 @@ set -e
 
 cd $(dirname $0)
 
+tag_name=`curl -s https://api.github.com/repos/protocolbuffers/protobuf/releases/latest | grep tag_name | cut -d '"' -f 4`
+echo "updating protoc binaries to version $tag_name" >&2
+
 for arch in linux-aarch_64 linux-ppcle_64 linux-x86_32 linux-x86_64 osx-x86_64 win32; do
-    tag_name=`curl -s https://api.github.com/repos/protocolbuffers/protobuf/releases/latest | grep tag_name | cut -d '"' -f 4`
     TMPFILE=`mktemp`
     curl -sL https://github.com/protocolbuffers/protobuf/releases/download/${tag_name}/protoc-${tag_name#v}-${arch}.zip --output ${TMPFILE}.zip
     if [[ $arch == "win32" ]]; then
