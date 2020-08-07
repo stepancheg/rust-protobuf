80d8e3f439c6fcf851188d2974e490994f1c0e3d
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-08-07 06:06:24 +0100

    Inline several trivial functions

--- protobuf/src/reflect/acc/mod.rs
+++ protobuf/src/reflect/acc/mod.rs
@@ -1,10 +1,8 @@
 #![doc(hidden)]
 
-use crate::reflect::acc::v1::FieldAccessorImpl;
 use crate::reflect::acc::v1::FieldAccessorFunctions;
+use crate::reflect::acc::v1::FieldAccessorImpl;
 use crate::reflect::acc::v1::FieldAccessorTrait;
-use crate::reflect::EnumValueDescriptor;
-use crate::reflect::ReflectFieldRef;
 use crate::Message;
 
 pub(crate) mod v1;
@@ -28,79 +26,6 @@ impl FieldAccessor {
         FieldAccessor {
             name,
             accessor: Accessor::V1(Box::new(FieldAccessorImpl { fns })),
-        }
-    }
-
-    pub(crate) fn has_field_generic(&self, m: &dyn Message) -> bool {
-        match &self.accessor {
-            Accessor::V1(a) => a.has_field_generic(m),
-        }
-    }
-
-    pub(crate) fn len_field_generic(&self, m: &dyn Message) -> usize {
-        match &self.accessor {
-            Accessor::V1(a) => a.len_field_generic(m),
-        }
-    }
-    pub(crate) fn get_message_generic<'a>(&self, m: &'a dyn Message) -> &'a dyn Message {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_message_generic(m),
-        }
-    }
-    pub(crate) fn get_enum_generic(&self, m: &dyn Message) -> &'static EnumValueDescriptor {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_enum_generic(m),
-        }
-    }
-    pub(crate) fn get_str_generic<'a>(&self, m: &'a dyn Message) -> &'a str {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_str_generic(m),
-        }
-    }
-    pub(crate) fn get_bytes_generic<'a>(&self, m: &'a dyn Message) -> &'a [u8] {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_bytes_generic(m),
-        }
-    }
-    pub(crate) fn get_u32_generic(&self, m: &dyn Message) -> u32 {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_u32_generic(m),
-        }
-    }
-    pub(crate) fn get_u64_generic(&self, m: &dyn Message) -> u64 {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_u64_generic(m),
-        }
-    }
-    pub(crate) fn get_i32_generic(&self, m: &dyn Message) -> i32 {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_i32_generic(m),
-        }
-    }
-    pub(crate) fn get_i64_generic(&self, m: &dyn Message) -> i64 {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_i64_generic(m),
-        }
-    }
-    pub(crate) fn get_bool_generic(&self, m: &dyn Message) -> bool {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_bool_generic(m),
-        }
-    }
-    pub(crate) fn get_f32_generic(&self, m: &dyn Message) -> f32 {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_f32_generic(m),
-        }
-    }
-    pub(crate) fn get_f64_generic(&self, m: &dyn Message) -> f64 {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_f64_generic(m),
-        }
-    }
-
-    pub(crate) fn get_reflect<'a>(&self, m: &'a dyn Message) -> ReflectFieldRef<'a> {
-        match &self.accessor {
-            Accessor::V1(a) => a.get_reflect(m),
         }
     }
 }
