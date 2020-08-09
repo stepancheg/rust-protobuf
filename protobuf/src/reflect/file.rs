a5df3d67ed5e1cd9ac5759c2cebb26ffe3aaebb6
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-08-09 01:06:41 +0100

    No longer used descriptor

--- protobuf/src/reflect/file.rs
+++ protobuf/src/reflect/file.rs
@@ -15,21 +15,6 @@ pub struct FileDescriptor {
 
 impl FileDescriptor {
     /// This function is to be called from generated code.
-    pub fn new(
-        file_descriptor_proto: &'static FileDescriptorProto,
-        dependencies: Vec<&'static FileDescriptor>,
-        messages: Vec<&'static MessageDescriptor>,
-        enums: Vec<&'static EnumDescriptor>,
-    ) -> FileDescriptor {
-        FileDescriptor {
-            file_descriptor_proto: ArcOrStatic::Static(file_descriptor_proto),
-            dependencies: dependencies.into_iter().map(ArcOrStatic::Static).collect(),
-            messages,
-            enums,
-        }
-    }
-
-    /// This function is to be called from generated code.
     pub fn new_generated(
         file_descriptor_proto: &'static FileDescriptorProto,
         dependencies: Vec<&'static FileDescriptor>,
