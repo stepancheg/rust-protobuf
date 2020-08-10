6d6cae090944b2fa5c363634058a1d33cf2769e7
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-08-10 05:25:46 +0100

    RuntimeTypeDynamic::default_value_ref is dead code

--- protobuf/src/reflect/runtime_type_dynamic.rs
+++ protobuf/src/reflect/runtime_type_dynamic.rs
@@ -2,7 +2,6 @@ use std::marker;
 
 use crate::reflect::runtime_type_box::RuntimeTypeBox;
 use crate::reflect::runtime_types::RuntimeType;
-use crate::reflect::ReflectValueRef;
 
 /// Dynamic version of `RuntimeType`.
 ///
@@ -10,9 +9,6 @@ use crate::reflect::ReflectValueRef;
 pub trait RuntimeTypeDynamic: Send + Sync + 'static {
     /// Convert to "enum" version
     fn to_box(&self) -> RuntimeTypeBox;
-
-    /// Default value for type
-    fn default_value_ref(&self) -> ReflectValueRef;
 }
 
 pub(crate) struct RuntimeTypeDynamicImpl<T: RuntimeType>(pub marker::PhantomData<T>);
@@ -20,9 +16,5 @@ pub(crate) struct RuntimeTypeDynamicImpl<T: RuntimeType>(pub marker::PhantomData
 impl<T: RuntimeType> RuntimeTypeDynamic for RuntimeTypeDynamicImpl<T> {
     fn to_box(&self) -> RuntimeTypeBox {
         T::runtime_type_box()
-    }
-
-    fn default_value_ref(&self) -> ReflectValueRef {
-        T::default_value_ref()
     }
 }
