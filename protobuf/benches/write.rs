2aac6d9428dea7393c1d8d3ba92dbe5f095c1f40
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-08-10 11:13:59 +0100

    Simple bencher for write_to_bytes
    test write_to_bytes ... bench:         198 ns/iter (+/- 23)
    
    now

---
+++ protobuf/benches/write.rs
@@ -0,0 +1,23 @@
+// `cargo test --benches` and `#[feature(test)]` work only in nightly
+#![cfg(rustc_nightly)]
+#![feature(test)]
+
+extern crate test;
+
+use protobuf::well_known_types::value;
+use protobuf::well_known_types::Struct;
+use protobuf::well_known_types::Value;
+use protobuf::Message;
+use test::Bencher;
+
+#[bench]
+fn write_to_bytes(b: &mut Bencher) {
+    let mut value = Value::new();
+    value.kind = Some(value::Kind::number_value(10.0));
+    let mut value2 = Value::new();
+    value2.kind = Some(value::Kind::bool_value(true));
+    let mut s = Struct::new();
+    s.fields.insert("foo".to_owned(), value);
+    s.fields.insert("bar".to_owned(), value2);
+    b.iter(|| s.write_to_bytes());
+}
