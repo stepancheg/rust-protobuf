3f35524cfc3f2c2a0d125babf7ae1e12ae1cff9d
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-08-17 22:36:20 +0100

    Test generated getters

---
+++ protobuf-test/src/common/v2/test_message_getter.rs
@@ -0,0 +1,8 @@
+use super::test_message_getter_pb::MessageForTestGetter;
+
+#[test]
+fn get_returns_default_value() {
+    let mut m = MessageForTestGetter::new();
+    assert_eq!(0, m.get_i());
+    assert_eq!(false, m.get_b());
+}
