ca2223e627ec9a78335b5169244cf2e2b10512b0
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-08-17 22:53:07 +0100

    unnecessary mut in test

--- protobuf-test/src/common/v2/test_message_getter.rs
+++ protobuf-test/src/common/v2/test_message_getter.rs
@@ -2,7 +2,7 @@ use super::test_message_getter_pb::MessageForTestGetter;
 
 #[test]
 fn get_returns_default_value() {
-    let mut m = MessageForTestGetter::new();
+    let m = MessageForTestGetter::new();
     assert_eq!(0, m.get_i());
     assert_eq!(false, m.get_b());
 }
