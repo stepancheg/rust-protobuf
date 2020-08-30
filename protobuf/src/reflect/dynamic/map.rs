1bd50036877b16dc0c618ae1bce751c8d6ffbb4d
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-08-30 02:52:03 +0100

    Do not allocate in DynamicMap.get

--- protobuf/src/reflect/dynamic/map.rs
+++ protobuf/src/reflect/dynamic/map.rs
@@ -7,18 +7,80 @@ use crate::reflect::RuntimeTypeBox;
 use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
+enum Maps {
+    U32(HashMap<u32, ReflectValueBox>),
+    I32(HashMap<i32, ReflectValueBox>),
+    U64(HashMap<u64, ReflectValueBox>),
+    I64(HashMap<i64, ReflectValueBox>),
+    Bool(HashMap<bool, ReflectValueBox>),
+    String(HashMap<String, ReflectValueBox>),
+}
+
+impl Maps {
+    fn len(&self) -> usize {
+        match self {
+            Maps::U32(m) => m.len(),
+            Maps::I32(m) => m.len(),
+            Maps::U64(m) => m.len(),
+            Maps::I64(m) => m.len(),
+            Maps::Bool(m) => m.len(),
+            Maps::String(m) => m.len(),
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        match self {
+            Maps::U32(m) => m.is_empty(),
+            Maps::I32(m) => m.is_empty(),
+            Maps::U64(m) => m.is_empty(),
+            Maps::I64(m) => m.is_empty(),
+            Maps::Bool(m) => m.is_empty(),
+            Maps::String(m) => m.is_empty(),
+        }
+    }
+
+    fn clear(&mut self) {
+        match self {
+            Maps::U32(m) => m.clear(),
+            Maps::I32(m) => m.clear(),
+            Maps::U64(m) => m.clear(),
+            Maps::I64(m) => m.clear(),
+            Maps::Bool(m) => m.clear(),
+            Maps::String(m) => m.clear(),
+        }
+    }
+
+    fn key_type(&self) -> RuntimeTypeBox {
+        match self {
+            Maps::U32(..) => RuntimeTypeBox::U32,
+            Maps::I32(..) => RuntimeTypeBox::I32,
+            Maps::U64(..) => RuntimeTypeBox::U64,
+            Maps::I64(..) => RuntimeTypeBox::I64,
+            Maps::Bool(..) => RuntimeTypeBox::Bool,
+            Maps::String(..) => RuntimeTypeBox::String,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub(crate) struct DynamicMap {
-    key: RuntimeTypeBox,
     value: RuntimeTypeBox,
-    map: HashMap<ReflectValueBoxHashable, ReflectValueBox>,
+    maps: Maps,
 }
 
 impl DynamicMap {
     pub fn new(key: RuntimeTypeBox, value: RuntimeTypeBox) -> DynamicMap {
         DynamicMap {
-            key,
             value,
-            map: HashMap::new(),
+            maps: match key {
+                RuntimeTypeBox::U32 => Maps::U32(HashMap::new()),
+                RuntimeTypeBox::I32 => Maps::I32(HashMap::new()),
+                RuntimeTypeBox::U64 => Maps::U64(HashMap::new()),
+                RuntimeTypeBox::I64 => Maps::I64(HashMap::new()),
+                RuntimeTypeBox::Bool => Maps::Bool(HashMap::new()),
+                RuntimeTypeBox::String => Maps::String(HashMap::new()),
+                t => panic!("type cannot be hashmap key: {}", t),
+            },
         }
     }
 }
@@ -30,39 +92,48 @@ impl ReflectMap for DynamicMap {
     }
 
     fn len(&self) -> usize {
-        self.map.len()
+        self.maps.len()
     }
 
     fn is_empty(&self) -> bool {
-        self.map.is_empty()
+        self.maps.is_empty()
     }
 
     fn get<'a>(&'a self, key: ReflectValueRef) -> Option<ReflectValueRef<'a>> {
-        if key.get_type() != self.key {
-            return None;
-        }
-
-        match key {
-            ReflectValueRef::String(s) => self.map.get(s),
-            key => self
-                .map
-                .get(&ReflectValueBoxHashable::from_box(key.to_box())),
+        match (&self.maps, key) {
+            (Maps::U32(m), ReflectValueRef::U32(v)) => m.get(&v),
+            (Maps::U64(m), ReflectValueRef::U64(v)) => m.get(&v),
+            (Maps::I32(m), ReflectValueRef::I32(v)) => m.get(&v),
+            (Maps::I64(m), ReflectValueRef::I64(v)) => m.get(&v),
+            (Maps::Bool(m), ReflectValueRef::Bool(v)) => m.get(&v),
+            (Maps::String(m), ReflectValueRef::String(v)) => m.get(&*v),
+            _ => None,
         }
         .map(ReflectValueBox::as_value_ref)
     }
 
     fn insert(&mut self, key: ReflectValueBoxHashable, value: ReflectValueBox) {
-        assert!(key.get_type() == self.key);
         assert!(value.get_type() == self.value);
-        self.map.insert(key, value);
+        match (&mut self.maps, &key) {
+            (Maps::U32(m), ReflectValueBoxHashable::U32(k)) => m.insert(*k, value),
+            (Maps::U64(m), ReflectValueBoxHashable::U64(k)) => m.insert(*k, value),
+            (Maps::I32(m), ReflectValueBoxHashable::I32(k)) => m.insert(*k, value),
+            (Maps::I64(m), ReflectValueBoxHashable::I64(k)) => m.insert(*k, value),
+            (Maps::Bool(m), ReflectValueBoxHashable::Bool(k)) => m.insert(*k, value),
+            (Maps::String(m), _) => match key {
+                ReflectValueBoxHashable::String(k) => m.insert(k, value),
+                _ => panic!("wrong key type"),
+            },
+            _ => panic!("wrong key type"),
+        };
     }
 
     fn clear(&mut self) {
-        self.map.clear()
+        self.maps.clear()
     }
 
     fn key_type(&self) -> RuntimeTypeBox {
-        self.key.clone()
+        self.maps.key_type()
     }
 
     fn value_type(&self) -> RuntimeTypeBox {
--- protobuf/src/reflect/value/hashable.rs
+++ protobuf/src/reflect/value/hashable.rs
@@ -2,10 +2,28 @@ use crate::reflect::ProtobufValue;
 use crate::reflect::ReflectValueBox;
 use crate::reflect::ReflectValueRef;
 use crate::reflect::RuntimeTypeBox;
-use std::borrow::Borrow;
+use std::hash::Hash;
+use std::hash::Hasher;
+
+/// Subset of [`ReflectValueRef`], only hashable types.
+#[derive(Eq, PartialEq, Hash, Debug, Clone)]
+pub enum ReflectValueRefHashable<'a> {
+    /// `u32`
+    U32(u32),
+    /// `u64`
+    U64(u64),
+    /// `i32`
+    I32(i32),
+    /// `i64`
+    I64(i64),
+    /// `f32`
+    Bool(bool),
+    /// `string`
+    String(&'a str),
+}
 
 /// Subset of [`ReflectValueBox`], only hashable types.
-#[derive(Eq, PartialEq, Hash, Debug, Clone)]
+#[derive(Eq, Debug, Clone)]
 pub enum ReflectValueBoxHashable {
     /// `u32`
     U32(u32),
@@ -19,6 +37,35 @@ pub enum ReflectValueBoxHashable {
     Bool(bool),
     /// `string`
     String(String),
+}
+
+impl PartialEq for ReflectValueBoxHashable {
+    fn eq(&self, other: &Self) -> bool {
+        self.as_ref() == other.as_ref()
+    }
+}
+
+impl Hash for ReflectValueBoxHashable {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        Hash::hash(&self.as_ref(), state)
+    }
+}
+
+impl<'a> ReflectValueRefHashable<'a> {
+    /// Try convert.
+    pub fn try_from_value_ref<'b>(
+        v: ReflectValueRef<'b>,
+    ) -> Result<ReflectValueRefHashable<'b>, ReflectValueRef<'b>> {
+        Ok(match v {
+            ReflectValueRef::U32(v) => ReflectValueRefHashable::U32(v),
+            ReflectValueRef::U64(v) => ReflectValueRefHashable::U64(v),
+            ReflectValueRef::I32(v) => ReflectValueRefHashable::I32(v),
+            ReflectValueRef::I64(v) => ReflectValueRefHashable::I64(v),
+            ReflectValueRef::Bool(v) => ReflectValueRefHashable::Bool(v),
+            ReflectValueRef::String(v) => ReflectValueRefHashable::String(v),
+            v => return Err(v),
+        })
+    }
 }
 
 impl ReflectValueBoxHashable {
@@ -40,6 +87,18 @@ impl ReflectValueBoxHashable {
         match Self::try_from(v) {
             Ok(v) => v,
             Err(v) => panic!("value is not hashable: {:?}", v),
+        }
+    }
+
+    /// Convert.
+    pub fn as_ref(&self) -> ReflectValueRefHashable {
+        match self {
+            ReflectValueBoxHashable::I32(v) => ReflectValueRefHashable::I32(*v),
+            ReflectValueBoxHashable::U32(v) => ReflectValueRefHashable::U32(*v),
+            ReflectValueBoxHashable::U64(v) => ReflectValueRefHashable::U64(*v),
+            ReflectValueBoxHashable::I64(v) => ReflectValueRefHashable::I64(*v),
+            ReflectValueBoxHashable::Bool(v) => ReflectValueRefHashable::Bool(*v),
+            ReflectValueBoxHashable::String(v) => ReflectValueRefHashable::String(v),
         }
     }
 
@@ -78,14 +137,16 @@ impl ReflectValueBoxHashable {
             .downcast()
             .map_err(ReflectValueBoxHashable::from_box)
     }
-}
 
-// TODO: implement hash consistently
-impl Borrow<str> for ReflectValueBoxHashable {
-    fn borrow(&self) -> &str {
+    /// Convert.
+    pub fn to_ref(&self) -> ReflectValueRefHashable {
         match self {
-            ReflectValueBoxHashable::String(s) => s.as_str(),
-            _ => panic!("not a str"),
+            ReflectValueBoxHashable::I32(v) => ReflectValueRefHashable::I32(*v),
+            ReflectValueBoxHashable::U32(v) => ReflectValueRefHashable::U32(*v),
+            ReflectValueBoxHashable::U64(v) => ReflectValueRefHashable::U64(*v),
+            ReflectValueBoxHashable::I64(v) => ReflectValueRefHashable::I64(*v),
+            ReflectValueBoxHashable::Bool(v) => ReflectValueRefHashable::Bool(*v),
+            ReflectValueBoxHashable::String(v) => ReflectValueRefHashable::String(v),
         }
     }
 }
