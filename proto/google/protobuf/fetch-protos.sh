0d6ebbb42c82578d116f30f295d89f86b5b12b7d
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-09-28 02:31:34 +0100

    No longer filter out `reserved` keyword in bundled protos
    Since we are no longer compatible with 2.6 protoc

--- proto/google/protobuf/fetch-protos.sh
+++ proto/google/protobuf/fetch-protos.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -ex
 
 # fetch protos from protobuf respository
-# comment out `reserved` directive for comatibility with protobuf 2.6.1
-# https://github.com/google/protobuf/issues/1669#issuecomment-240598089
 for f in descriptor.proto compiler/plugin.proto \
     any.proto \
     api.proto \
@@ -16,7 +14,6 @@ for f in descriptor.proto compiler/plugin.proto \
     wrappers.proto \
 ; do
     curl -s https://raw.githubusercontent.com/google/protobuf/v3.1.0/src/google/protobuf/$f \
-        | sed -e 's,^\( *\)\(reserved.*\),\1// \2,' \
         > $f
 done
 
