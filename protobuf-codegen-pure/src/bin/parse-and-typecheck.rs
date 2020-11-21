53b221e90ccf7bc8c105c4b07d3f4340f9e601dd
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-11-21 19:48:24 +0000

    parse-and-typecheck: better error message on incorrect input

--- protobuf-codegen-pure/src/bin/parse-and-typecheck.rs
+++ protobuf-codegen-pure/src/bin/parse-and-typecheck.rs
@@ -2,10 +2,19 @@ extern crate protobuf_codegen_pure;
 
 use std::env;
 use std::path::Path;
+use std::process::exit;
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    assert_eq!(2, args.len());
+
+    if args.len() != 2 {
+        eprintln!(
+            "usage: {} <input.proto> <include>",
+            env::args().next().unwrap()
+        );
+        exit(1);
+    }
+
     let includes = vec![Path::new(&args[0][..])];
     let input = vec![Path::new(&args[1][..])];
     let t =
