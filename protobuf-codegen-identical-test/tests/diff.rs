dfdd51663d58c2fe7f3bcbd3758c78c592e9cc07
Stiopa Koltsov <stepan.koltsov@gmail.com>
2020-11-22 02:41:00 +0000

    Compare descriptors

--- protobuf-codegen-identical-test/tests/diff.rs
+++ protobuf-codegen-identical-test/tests/diff.rs
@@ -1,13 +1,26 @@
 use regex::Regex;
+use std::collections::HashSet;
+use std::fmt;
 use std::fmt::Write as _;
 use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
 use std::path::MAIN_SEPARATOR;
 use std::str;
+use tempfile::NamedTempFile;
 
+use protobuf::descriptor::DescriptorProto;
+use protobuf::descriptor::EnumDescriptorProto;
+use protobuf::descriptor::FieldDescriptorProto;
+use protobuf::descriptor::FieldDescriptorProto_Type;
+use protobuf::descriptor::FileDescriptorProto;
+use protobuf::descriptor::FileDescriptorSet;
+use protobuf::descriptor::OneofDescriptorProto;
+use protobuf::parse_from_reader;
+use protobuf_codegen::float::parse_protobuf_float;
 use protobuf_test_common::build::copy_tests_v2_v3;
 use protobuf_test_common::build::glob_simple;
+use protoc::DescriptorSetOutArgs;
 use std::process::Command;
 use std::process::Stdio;
 
@@ -66,6 +79,14 @@ fn print_diff(dir: &Path, a: &Path, b: &Path) {
         return;
     }
 
+    // sanity check
+    let mut a_full = dir.to_path_buf();
+    a_full.push(a);
+    assert!(a_full.exists());
+    let mut b_full = dir.to_path_buf();
+    b_full.push(b);
+    assert!(b_full.exists());
+
     // Not using difference crate because it's slow for large files
     let output = Command::new("diff")
         .current_dir(dir)
@@ -82,6 +103,166 @@ fn print_diff(dir: &Path, a: &Path, b: &Path) {
     print!("{}", str::from_utf8(&output.stderr).unwrap());
 }
 
+fn protoc_descriptor_set(includes: &[PathBuf], inputs: &[PathBuf]) -> FileDescriptorSet {
+    let mut temp_file = NamedTempFile::new().unwrap();
+    let includes: Vec<&str> = includes.iter().map(|p| p.to_str().unwrap()).collect();
+    let inputs: Vec<&str> = inputs.iter().map(|p| p.to_str().unwrap()).collect();
+    protoc::Protoc::from_path(protoc_bin_vendored::protoc_bin_path().unwrap())
+        .write_descriptor_set(DescriptorSetOutArgs {
+            out: temp_file.path().to_str().unwrap(),
+            includes: &includes,
+            input: &inputs,
+            include_imports: false,
+        })
+        .unwrap();
+    parse_from_reader(&mut temp_file).unwrap()
+}
+
+// TODO: expose this utility from protobuf-codegen-pure crate.
+fn pure_descriptor_set(includes: &[PathBuf], inputs: &[PathBuf]) -> FileDescriptorSet {
+    let includes: Vec<&Path> = includes.iter().map(|p| p.as_path()).collect();
+    let inputs: Vec<&Path> = inputs.iter().map(|p| p.as_path()).collect();
+    let mut codegen = protobuf_codegen_pure::parse_and_typecheck(&includes, &inputs).unwrap();
+    let relative_paths: HashSet<_> = codegen.relative_paths.iter().map(|s| s.as_str()).collect();
+    codegen
+        .file_descriptors
+        .retain(|fd| relative_paths.contains(fd.get_name()));
+    let mut fds = FileDescriptorSet::new();
+    fds.file = codegen.file_descriptors.into();
+    fds
+}
+
+fn normalize_descriptor_set(fds: &mut FileDescriptorSet) {
+    for desc in fds.file.as_mut() {
+        normalize_file_descriptor(desc)
+    }
+}
+
+fn normalize_file_descriptor(desc: &mut FileDescriptorProto) {
+    if !desc.has_syntax() {
+        desc.set_syntax("proto2".into())
+    }
+    for desc in desc.message_type.as_mut() {
+        normalize_descriptor(desc)
+    }
+    for desc in desc.enum_type.as_mut() {
+        normalize_enum_descriptor(desc)
+    }
+
+    for desc in desc.extension.as_mut() {
+        desc.mut_options();
+    }
+    desc.mut_options();
+
+    // TODO: don't clear services.
+    desc.service.clear();
+
+    // TODO: do not clear
+    desc.dependency.clear();
+
+    // for unittest_custom_options.proto where a custom option
+    // is an extension. Probably nobody outside of Google uses it.
+    desc.mut_options().unknown_fields.remove(15478479);
+}
+
+fn normalize_enum_descriptor(desc: &mut EnumDescriptorProto) {
+    desc.mut_options();
+
+    for value in desc.value.as_mut() {
+        value.mut_options();
+    }
+}
+
+fn normalize_oneof_descriptor(desc: &mut OneofDescriptorProto) {
+    desc.mut_options();
+}
+
+fn normalize_descriptor(desc: &mut DescriptorProto) {
+    // TODO: implement IntoIterator for &mut RepeatedField
+    for desc in desc.nested_type.as_mut() {
+        normalize_descriptor(desc);
+    }
+    for desc in desc.enum_type.as_mut() {
+        normalize_enum_descriptor(desc);
+    }
+    for desc in desc.oneof_decl.as_mut() {
+        normalize_oneof_descriptor(desc);
+    }
+
+    // TODO: don't clear options.
+    desc.options.clear();
+
+    for field in desc.field.as_mut() {
+        normalize_field(field);
+    }
+
+    for ext in desc.extension.as_mut() {
+        ext.mut_options();
+    }
+
+    for ext in desc.extension_range.as_mut() {
+        // If ext range end exceeds max field number,
+        // the actual upper limit does not matter.
+        // protoc is not consistent in behavior thus flush
+        // the value to some arbitrary compatible value.
+        if ext.has_end() && ext.get_end() >= 0x20000000 {
+            ext.set_end(0x20000000);
+        }
+    }
+}
+
+fn normalize_field(field: &mut FieldDescriptorProto) {
+    field.mut_options();
+
+    if field.has_default_value() {
+        match field.get_field_type() {
+            FieldDescriptorProto_Type::TYPE_FLOAT | FieldDescriptorProto_Type::TYPE_DOUBLE => {
+                field.set_default_value(format!(
+                    "{}",
+                    parse_protobuf_float(field.get_default_value()).unwrap()
+                ));
+            }
+            FieldDescriptorProto_Type::TYPE_STRING | FieldDescriptorProto_Type::TYPE_BYTES => {
+                // TODO: fix string default values
+                field.clear_default_value();
+            }
+            _ => {}
+        }
+    }
+}
+
+fn pretty_message<M: protobuf::Message>(message: &M) -> String {
+    // TODO: pull this handy utility into the protobuf crate.
+    struct FormatMessage<'a, M>(&'a M);
+
+    impl<M: protobuf::Message> fmt::Display for FormatMessage<'_, M> {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            protobuf::text_format::fmt(self.0, f)
+        }
+    }
+
+    format!("{:#}", FormatMessage(message))
+}
+
+fn descriptor_for_file<'a>(fds: &'a FileDescriptorSet, file_name: &str) -> &'a FileDescriptorProto {
+    for file in &fds.file {
+        if Path::new(file.get_name()).file_name().unwrap()
+            == Path::new(file_name).file_name().unwrap()
+        {
+            return file;
+        }
+    }
+    panic!(
+        "file not found: {}; all files: {}",
+        file_name,
+        fds.file
+            .iter()
+            .map(|f| f.get_name())
+            .collect::<Vec<_>>()
+            .join(", ")
+    );
+}
+
 fn test_diff_in<F>(root: &str, sources_dir: &str, include: &str, should_skip: F)
 where
     F: Fn(&str) -> bool,
@@ -93,10 +274,13 @@ where
         failed: 0,
     };
 
+    let mut include_root = Path::new(root).to_path_buf();
+    include_root.push(include);
+
     let include_full = format!("{}/{}", root, include);
     let s_full = format!("{}/{}", root, sources_dir);
 
-    let inputs_glob = format!("{}/*.proto", s_full);
+    let inputs_glob = format!("{}/*.proto*", s_full);
     let inputs = to_paths(glob_simple(&inputs_glob));
     assert!(!inputs.is_empty(), "glob is empty: {}", inputs_glob);
     let includes = to_paths(vec![include_full.as_str(), "../proto"]);
@@ -127,8 +311,14 @@ where
         .run()
         .unwrap();
 
+    let mut protoc_descriptors = protoc_descriptor_set(&includes, &inputs);
+    let mut pure_descriptors = pure_descriptor_set(&includes, &inputs);
+    normalize_descriptor_set(&mut protoc_descriptors);
+    normalize_descriptor_set(&mut pure_descriptors);
+
     for input in &inputs {
         let label = input.strip_prefix(root).unwrap().to_str().unwrap();
+        let proto_file_name = input.strip_prefix(&include_root).unwrap().to_str().unwrap();
         let proto_name = input.file_name().unwrap().to_str().unwrap();
         let rs_name = protobuf_codegen::proto_name_to_rs(proto_name);
         let protoc_rs = format!("{}/{}", protoc_dir, rs_name);
@@ -141,8 +331,14 @@ where
             fs::read_to_string(&protoc_rs).expect(&format!("while reading {}", protoc_rs));
         let pure_rs_contents =
             fs::read_to_string(&pure_rs).expect(&format!("while reading {}", pure_rs));
+
+        let protoc_descriptor_for_file = descriptor_for_file(&protoc_descriptors, &proto_file_name);
+        let pure_descriptor_for_file = descriptor_for_file(&pure_descriptors, &proto_file_name);
+
         let skip = should_skip(input.to_str().unwrap());
-        if protoc_rs_contents == pure_rs_contents {
+        if protoc_rs_contents == pure_rs_contents
+            && protoc_descriptor_for_file == pure_descriptor_for_file
+        {
             if !skip {
                 stats.passed += 1;
                 println!("{}: PASSED", label);
@@ -163,6 +359,22 @@ where
                 temp_dir.path(),
                 Path::new(&protoc_rs).strip_prefix(temp_dir.path()).unwrap(),
                 Path::new(&pure_rs).strip_prefix(temp_dir.path()).unwrap(),
+            );
+
+            fs::write(
+                format!("{}/{}.descriptors", protoc_dir, proto_name),
+                pretty_message(protoc_descriptor_for_file),
+            )
+            .unwrap();
+            fs::write(
+                format!("{}/{}.descriptors", pure_dir, proto_name),
+                pretty_message(pure_descriptor_for_file),
+            )
+            .unwrap();
+            print_diff(
+                temp_dir.path(),
+                Path::new(&format!("protoc/{}.descriptors", proto_name)),
+                Path::new(&format!("pure/{}.descriptors", proto_name)),
             );
         }
     }
