11c1d69b08ca2e8df19fc24feb10556beb8ca607
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-02-06 00:41:00 +0000

    Specify protobuf version env for rustfmt job

--- ci-gen/src/install_protobuf.rs
+++ ci-gen/src/install_protobuf.rs
@@ -2,7 +2,7 @@ use crate::ghwf::Step;
 
 pub fn install_protobuf() -> Vec<Step> {
     vec![
-        Step::run("Install protobuf", "ci/install-protobuf.sh"),
+        Step::run("Install protobuf", "ci/install-protobuf.sh").env("PROTOBUF_VERSION", "3.6.1"),
         Step::run("Protoc check", "protoc --version"),
     ]
 }
