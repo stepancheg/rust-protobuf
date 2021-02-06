7badc996d9ab55b0b5b15b896ff71666e569ef50
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-02-06 00:51:13 +0000

    Cache protobuf in rustfmt job

--- ci-gen/src/install_protobuf.rs
+++ ci-gen/src/install_protobuf.rs
@@ -1,8 +1,24 @@
+use crate::actions::cache;
 use crate::ghwf::Step;
+use crate::Os;
+use crate::MACOS;
+use crate::WINDOWS;
 
-pub fn install_protobuf() -> Vec<Step> {
-    vec![
+pub(crate) fn install_protobuf(os: Os) -> Vec<Step> {
+    let mut steps = Vec::new();
+    steps.push(cache(
+        "Cache protobuf",
+        &format!("pb-{}{}", os.name, if os == WINDOWS { "-1" } else { "" }),
+        "~/pb",
+    ));
+
+    if os == MACOS {
+        steps.push(Step::run("Install pkg-config", "brew install pkg-config"));
+    }
+
+    steps.push(
         Step::run("Install protobuf", "ci/install-protobuf.sh").env("PROTOBUF_VERSION", "3.6.1"),
-        Step::run("Protoc check", "protoc --version"),
-    ]
+    );
+    steps.push(Step::run("Protoc check", "protoc --version"));
+    steps
 }
