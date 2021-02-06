0cce945c0c698f963db4c1c20cd24bfcd5c179b8
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-02-06 00:17:24 +0000

    Switch protobuf-fuzz to pure-rust codegen

--- protobuf-fuzz/build.rs
+++ protobuf-fuzz/build.rs
@@ -1,10 +1,7 @@
-extern crate protobuf_test_common;
-extern crate protoc_rust;
-
 fn main() {
     protobuf_test_common::build::clean_old_files();
 
-    protoc_rust::Codegen::new()
+    protobuf_codegen_pure::Codegen::new()
         .out_dir("src")
         .include("src")
         .input("src/all_types_pb.proto")
