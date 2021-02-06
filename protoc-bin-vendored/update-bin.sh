831371f1fd70b4d6686959df707ece8bbe9ed741
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-02-06 02:06:43 +0000

    Update bundled protoc to the latest version
    Also write version number to version.txt

--- protoc-bin-vendored/update-bin.sh
+++ protoc-bin-vendored/update-bin.sh
@@ -5,6 +5,7 @@ set -e
 cd $(dirname $0)
 
 tag_name=`curl -s https://api.github.com/repos/protocolbuffers/protobuf/releases/latest | grep tag_name | cut -d '"' -f 4`
+echo "$tag_name" > version.txt
 echo "updating protoc binaries to version $tag_name" >&2
 
 for arch in linux-aarch_64 linux-ppcle_64 linux-x86_32 linux-x86_64 osx-x86_64 win32; do
