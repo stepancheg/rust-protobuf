26075bd92c8e6ed89c040bfd2ff5a323c5e81a30
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-02-08 03:40:11 +0000

    Utility to benchmark read performance

---
+++ perftest/misc/build.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::io::Read;
+use std::process;
+
+// % rustc +stable --version
+// rustc 1.26.0 (a77568041 2018-05-07)
+// % rustc +beta --version
+// rustc 1.27.0-beta.1 (03fb2f447 2018-05-09)
+// % rustc +nightly --version
+// rustc 1.27.0-nightly (acd3871ba 2018-05-10)
+fn version_is_nightly(version: &str) -> bool {
+    version.contains("nightly")
+}
+
+fn export_rustc_cfg() {
+    let rustc = env::var("RUSTC").expect("RUSTC unset");
+
+    let mut child = process::Command::new(rustc)
+        .args(&["--version"])
+        .stdin(process::Stdio::null())
+        .stdout(process::Stdio::piped())
+        .spawn()
+        .expect("spawn rustc");
+
+    let mut rustc_version = String::new();
+
+    child
+        .stdout
+        .as_mut()
+        .expect("stdout")
+        .read_to_string(&mut rustc_version)
+        .expect("read_to_string");
+    assert!(child.wait().expect("wait").success());
+
+    if version_is_nightly(&rustc_version) {
+        println!("cargo:rustc-cfg=rustc_nightly");
+    }
+}
+
+fn main() {
+    export_rustc_cfg();
+}
---
+++ perftest/misc/src/bin/read_varint.rs
@@ -0,0 +1,53 @@
+use protobuf::CodedInputStream;
+use protobuf::CodedOutputStream;
+use protobuf_perftest_misc::black_box;
+use std::env;
+use std::process;
+use std::time::Instant;
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 3 {
+        eprintln!(
+            "usage: {} <limit> <count> <iter>",
+            env::args().next().unwrap()
+        );
+        process::exit(1);
+    }
+
+    let upper: u64 = args[0].parse().unwrap();
+    let count = args[1].parse().unwrap();
+    let iter = args[2].parse().unwrap();
+
+    let rng = fastrand::Rng::with_seed(17);
+
+    let mut data = Vec::new();
+    let mut os = CodedOutputStream::vec(&mut data);
+    for _ in 0..count {
+        let v = match upper {
+            0 => rng.u64(..),
+            x => rng.u64(..x),
+        };
+        os.write_raw_varint64(v).unwrap();
+    }
+    os.flush().unwrap();
+    drop(os);
+
+    let br = iter / 10;
+
+    let start = Instant::now();
+
+    for i in 0..iter {
+        if i % br == 0 {
+            eprintln!("{}", i / br);
+        }
+        let mut is = CodedInputStream::from_bytes(black_box(&data));
+        while !is.eof().unwrap() {
+            black_box(is.read_raw_varint64().unwrap());
+        }
+    }
+
+    let elapsed = start.elapsed();
+    let per_read = elapsed.as_secs_f64() / (iter as f64) / (count as f64);
+    eprintln!("{:.3}ns per iter", per_read * 1_000_000_000.0);
+}
