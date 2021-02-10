9a245a576f310556066100c8c2988998cc71f47d
Stiopa Koltsov <stepan.koltsov@gmail.com>
2021-02-10 20:09:32 +0000

    impl Display for ProtobufPath
    (internal change)

--- protobuf-codegen/src/protobuf_path.rs
+++ protobuf-codegen/src/protobuf_path.rs
@@ -1,8 +1,18 @@
 use crate::ProtobufAbsolutePath;
 use crate::ProtobufRelativePath;
+use std::fmt;
 
 /// Protobuf identifier can be absolute or relative.
 pub enum ProtobufPath {
     Abs(ProtobufAbsolutePath),
     Rel(ProtobufRelativePath),
+}
+
+impl fmt::Display for ProtobufPath {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ProtobufPath::Abs(p) => write!(f, "{}", p),
+            ProtobufPath::Rel(_) => write!(f, "{}", p),
+        }
+    }
 }
