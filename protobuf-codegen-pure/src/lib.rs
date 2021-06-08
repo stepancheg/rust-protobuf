61a96fde903d40a0b1ea804e1ce18877907b9a2a
Alexander Zaitsev <zamazan4ik@tut.by>
2021-06-08 23:49:16 +0300

    doc: fix documentaion
    - just a veeeeery small fix in the documentation

--- protobuf-codegen-pure/src/lib.rs
+++ protobuf-codegen-pure/src/lib.rs
@@ -8,7 +8,7 @@
 //! fn main() {
 //!     protobuf_codegen_pure::Codegen::new()
 //!         .out_dir("src/protos")
-//!         .inputs(&["protos/a.proto", "protos/b.proto"]),
+//!         .inputs(&["protos/a.proto", "protos/b.proto"])
 //!         .include("protos")
 //!         .run()
 //!         .expect("Codegen failed.");
