653d99a3134965303e96b3bcbb86144308bf998a
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-01 07:04:18 +0000

    rust-protobuf crate doc

--- protobuf/src/lib.rs
+++ protobuf/src/lib.rs
@@ -1,4 +1,25 @@
 //! Library to read and write protocol buffers data.
+//!
+//! # Version 2 is stable
+//!
+//! Currently developed branch of rust-protobuf is 3. It has the same spirit as version 2,
+//! but contains numerous improvements like:
+//! * runtime reflection for mutability, not just for access
+//! * protobuf text format and JSON parsing (which rely on reflection)
+//! * dynamic message support: work with protobuf data without generating code from schema
+//!
+//! Stable version of rust-protobuf will be supported until version 3 released.
+//!
+//! [Tracking issue for version 3](https://github.com/stepancheg/rust-protobuf/issues/518).
+//!
+//! # Accompanying crates
+//!
+//! * [`protobuf-codegen`](https://docs.rs/protobuf-codegen)
+//!   and [`protobuf-codegen-pure`](https://docs.rs/protobuf-codegen-pure)
+//!   can be used to rust code from `.proto` crates.
+//! * [`protoc`](https://docs.rs/protoc) crate can be used to invoke `protoc` programmatically.
+//! * [`protoc-bin-vendored`](https://docs.rs/protoc-bin-vendored) contains `protoc` command
+//!   packed into the crate.
 
 #![deny(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
