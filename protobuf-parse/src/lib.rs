2c0e6d91c3f869fd760b3cd878633bbe3016b322
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-01 06:37:23 +0000

    protobuf-parse crate docs

--- protobuf-parse/src/lib.rs
+++ protobuf-parse/src/lib.rs
@@ -2,6 +2,11 @@
 //!
 //! Files can be parsed using pure Rust parser (mod `pure`)
 //! or using the `protoc` command (mod `protoc`).
+//!
+//! This crate is not meant to be used directly, but rather through the `protobuf-codegen` crate.
+//! If you think this crate might be useful to you,
+//! please [consider creating an issue](https://github.com/stepancheg/rust-protobuf/issues/new),
+//! until that this crate is considered to have **no stable API**.
 
 mod case_convert;
 mod linked_hash_map;
