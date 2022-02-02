1bc2b2cd7d22a6c5ffbbc7f6b8454fb98c9f602d
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-02 10:30:35 +0000

    Better documentation of protoc-rust

--- protoc-rust/README.md
+++ protoc-rust/README.md
@@ -32,4 +32,17 @@ It is advisable that `protoc-rust` build-dependency version be the same as
 The alternative is to use
 [`protobuf-codegen-pure` crate](https://docs.rs/protobuf-codegen-pure).
 
+# Protoc binary
+
+This crate searches for `protoc` binary in `$PATH` by default.
+
+`protoc` binary can be obtained using
+[`protoc-bin-vendored` crate](https://docs.rs/protoc-bin-vendored)
+and supplied to `Codegen` object.
+
+# This is version 2
+
+In branch 3 of rust-protobuf this functionality is provided by
+[`protobuf-codegen` crate](https://docs.rs/protobuf-codegen/%3E=3.0.0-alpha).
+
 <!-- cargo-sync-readme end -->
--- protoc-rust/src/lib.rs
+++ protoc-rust/src/lib.rs
@@ -29,6 +29,19 @@
 //!
 //! The alternative is to use
 //! [`protobuf-codegen-pure` crate](https://docs.rs/protobuf-codegen-pure).
+//!
+//! # Protoc binary
+//!
+//! This crate searches for `protoc` binary in `$PATH` by default.
+//!
+//! `protoc` binary can be obtained using
+//! [`protoc-bin-vendored` crate](https://docs.rs/protoc-bin-vendored)
+//! and supplied to `Codegen` object.
+//!
+//! # This is version 2
+//!
+//! In branch 3 of rust-protobuf this functionality is provided by
+//! [`protobuf-codegen` crate](https://docs.rs/protobuf-codegen/%3E=3.0.0-alpha).
 
 #![deny(missing_docs)]
 #![deny(rustdoc::broken_intra_doc_links)]
