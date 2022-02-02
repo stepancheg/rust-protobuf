195129d619bd5c8b225450bbf9dc0d6a95bb00f7
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-02 10:22:24 +0000

    Fix test flakiness

--- protobuf-test-common/src/serialize_deserialize_both.rs
+++ protobuf-test-common/src/serialize_deserialize_both.rs
@@ -6,6 +6,7 @@ use crate::test_serialize_deserialize_no_hex;
 
 pub fn test_serialize_deserialize_no_hex_with_dynamic<M: Message + PartialEq>(m: &M) {
     test_serialize_deserialize_no_hex(m);
+    // TODO: compare messages instead of comparing hex
     serialize_and_parse_as_dynamic_and_serialize(m);
 }
 
