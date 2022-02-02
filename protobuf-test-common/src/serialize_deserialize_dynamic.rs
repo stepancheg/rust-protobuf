da339d4315323210f72f0f417c916f20fe5bd3fa
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-02 07:41:34 +0000

    Refactor tests

--- protobuf-test-common/src/serialize_deserialize_dynamic.rs
+++ protobuf-test-common/src/serialize_deserialize_dynamic.rs
@@ -1,20 +1,30 @@
 use protobuf::reflect::FileDescriptor;
+use protobuf::reflect::MessageDescriptor;
 use protobuf::Message;
 use protobuf::MessageDyn;
 
 use crate::hex::encode_hex;
 
-/// Serialize/deserialize test for `DynamicMessage`.
-pub fn serialize_and_parse_as_dynamic_and_serialize<M: Message>(m: &M) -> Box<dyn MessageDyn> {
-    // Recreate generated message file descriptor as dynamic descriptor.
+/// Recreate generated message file descriptor as dynamic descriptor.
+fn dynamic_descriptor_for_descriptor<M: Message>() -> MessageDescriptor {
     let deps = M::descriptor_static().file_descriptor().deps().to_vec();
-    let file_descriptor =
+    let dynamic_file_descriptor =
         FileDescriptor::new_dynamic(M::descriptor_static().file_descriptor_proto().clone(), deps);
 
     // Find the dynamic version of the generated message.
-    let description_dynamic = file_descriptor
+    let dynamic_descriptor = dynamic_file_descriptor
         .message_by_package_relative_name(M::descriptor_static().name_to_package())
         .unwrap();
+
+    // This descriptor is equivalent to `M::descriptor_static()`, but created dynamically
+    // using descriptor data stored in generated files.
+    dynamic_descriptor
+}
+
+/// Serialize/deserialize test for `DynamicMessage`.
+pub fn serialize_and_parse_as_dynamic_and_serialize<M: Message>(m: &M) -> Box<dyn MessageDyn> {
+    // Find the dynamic version of the generated message.
+    let description_dynamic = dynamic_descriptor_for_descriptor::<M>();
 
     // Serialize message as bytes.
     let bytes = m.write_to_bytes().unwrap();
