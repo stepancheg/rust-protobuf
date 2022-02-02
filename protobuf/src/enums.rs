d6c684aee4624ad828d6df93aa3aca5a3f9a4e49
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-02 04:48:50 +0000

    Revert "ProtobufEnum does not need to extend ProtobufValue"
    This reverts commit 383155522024e6f70ab45807a15a0883d931eb65.

--- protobuf/src/enums.rs
+++ protobuf/src/enums.rs
@@ -7,7 +7,7 @@ use crate::reflect::EnumValueDescriptor;
 use crate::reflect::ProtobufValue;
 
 /// Trait implemented by all protobuf enum types.
-pub trait ProtobufEnum: Eq + Sized + Copy + 'static + fmt::Debug + Default + Sync + Send {
+pub trait ProtobufEnum: Eq + Sized + Copy + 'static + ProtobufValue + fmt::Debug + Default {
     /// Get enum `i32` value.
     fn value(&self) -> i32;
 
