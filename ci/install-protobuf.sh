d679a8d87c6cfeb8be5a5b21bdcc0c1f3d8e5505
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-03 19:41:11 +0000

    shfmt install-protobuf.sh

--- ci/install-protobuf.sh
+++ ci/install-protobuf.sh
@@ -11,11 +11,11 @@ test -n "$PROTOBUF_VERSION" || die "PROTOBUF_VERSION env var is undefined"
 
 path_tr() {
     case $(uname) in
-        MINGW64*)
-            cygpath --mixed "$1"
+    MINGW64*)
+        cygpath --mixed "$1"
         ;;
-        *)
-            echo "$1"
+    *)
+        echo "$1"
         ;;
     esac
 }
@@ -23,10 +23,10 @@ path_tr() {
 test -n "$GITHUB_PATH"
 test -n "$GITHUB_ENV"
 
-path_tr "$HOME/pb/bin" >> "$GITHUB_PATH"
-echo "LD_LIBRARY_PATH=$(path_tr $HOME/pb/lib)" >> "$GITHUB_ENV"
-echo "PKG_CONFIG_PATH=$(path_tr $HOME/pb/lib/pkgconfig)" >> "$GITHUB_ENV"
-echo "PROTOBUF_PREFIX=$(path_tr $HOME/pb)" >> "$GITHUB_ENV"
+path_tr "$HOME/pb/bin" >>"$GITHUB_PATH"
+echo "LD_LIBRARY_PATH=$(path_tr $HOME/pb/lib)" >>"$GITHUB_ENV"
+echo "PKG_CONFIG_PATH=$(path_tr $HOME/pb/lib/pkgconfig)" >>"$GITHUB_ENV"
+echo "PROTOBUF_PREFIX=$(path_tr $HOME/pb)" >>"$GITHUB_ENV"
 
 if test -e "$HOME/pb/bin/protoc" -o -e "$HOME/pb/bin/protoc.exe"; then
     echo "Already exists"
@@ -48,40 +48,40 @@ rm -rf pb
 mkdir pb
 
 case $(uname) in
-    Linux|Darwin)
-        # Check we have ccache
-        # ccache --version
-        # export CC="ccache gcc"
-        # export CXX="ccache g++"
+Linux | Darwin)
+    # Check we have ccache
+    # ccache --version
+    # export CC="ccache gcc"
+    # export CXX="ccache g++"
 
-        case "$PROTOBUF_VERSION" in
-        2*)
-            basename=protobuf-$PROTOBUF_VERSION
-            ;;
-        3*)
-            basename=protobuf-cpp-$PROTOBUF_VERSION
-            ;;
-        *)
-            die "unknown protobuf version: $PROTOBUF_VERSION"
-            ;;
-        esac
-
-        curl -sL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz | tar zx
-
-        cd protobuf-$PROTOBUF_VERSION
-
-        ./configure --prefix=$HOME/pb && make -j2 && make install
-
-    ;;
-    MSYS_NT*|MINGW64*)
-        (
-            cd pb
-            curl -sLO https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/protoc-$PROTOBUF_VERSION-win32.zip
-            unzip protoc-$PROTOBUF_VERSION-win32.zip
-        )
-    ;;
+    case "$PROTOBUF_VERSION" in
+    2*)
+        basename=protobuf-$PROTOBUF_VERSION
+        ;;
+    3*)
+        basename=protobuf-cpp-$PROTOBUF_VERSION
+        ;;
     *)
-        die "unknown uname: `uname`"
+        die "unknown protobuf version: $PROTOBUF_VERSION"
+        ;;
+    esac
+
+    curl -sL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz | tar zx
+
+    cd protobuf-$PROTOBUF_VERSION
+
+    ./configure --prefix=$HOME/pb && make -j2 && make install
+
+    ;;
+MSYS_NT* | MINGW64*)
+    (
+        cd pb
+        curl -sLO https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/protoc-$PROTOBUF_VERSION-win32.zip
+        unzip protoc-$PROTOBUF_VERSION-win32.zip
+    )
+    ;;
+*)
+    die "unknown uname: $(uname)"
     ;;
 esac
 
