c0940009f99b301d851d19836fdbc794dc0507ca
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-06 10:54:55 +0000

    More renames carllerche-bytes -> tokio-bytes

--- protobuf-test/src/v2/test_tokio_bytes_default_value.rs
+++ protobuf-test/src/v2/test_tokio_bytes_default_value.rs
@@ -6,12 +6,12 @@ use super::test_tokio_bytes_default_value_pb::*;
 fn test_default_values() {
     assert_eq!(
         "sss",
-        TestCarllercheBytesDefaultValues::default_instance().get_s()
+        TestTokioBytesDefaultValues::default_instance().get_s()
     );
     assert_eq!(
         b"bbb",
-        TestCarllercheBytesDefaultValues::default_instance().get_b()
+        TestTokioBytesDefaultValues::default_instance().get_b()
     );
-    assert_eq!(&""[..], &**TestCarllercheBytesDefaultValues::new().mut_s());
-    assert_eq!(&b""[..], &**TestCarllercheBytesDefaultValues::new().mut_b());
+    assert_eq!(&""[..], &**TestTokioBytesDefaultValues::new().mut_s());
+    assert_eq!(&b""[..], &**TestTokioBytesDefaultValues::new().mut_b());
 }
