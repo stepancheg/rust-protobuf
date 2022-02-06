b6dd381abe838ae5647998c8f8bef2897e2bcba4
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-06 22:09:25 +0000

    Remove protoc-bin-vendored dev dependency on protoc

--- protoc-bin-vendored/tests/protoc-crate-compat.rs
+++
@@ -1,9 +0,0 @@
-#[test]
-fn protoc_crate_compat() {
-    assert!(
-        protoc::Protoc::from_path(protoc_bin_vendored::protoc_bin_path().unwrap())
-            .version()
-            .unwrap()
-            .is_3()
-    );
-}
