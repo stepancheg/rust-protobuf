7e9a81130814db15d8537c01922095ac38b40ae3
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-06 10:48:47 +0000

    More renames carllerche -> tokio

--- protobuf-test/src/common/v2/test_map_tokio.rs
+++ protobuf-test/src/common/v2/test_map_tokio.rs
@@ -6,7 +6,7 @@ use super::test_map_tokio_pb::*;
 
 #[test]
 fn test_string_to_int32() {
-    let mut map = TestMapCarllerche::new();
+    let mut map = TestMapTokio::new();
     map.string_to_int32.insert(Chars::from("abc"), 17);
     test_serialize_deserialize_with_dynamic("0a 07 0a 03 61 62 63 10 11", &map);
     //                          field 1, length-delimited
@@ -20,10 +20,10 @@ fn test_string_to_int32() {
 
 #[test]
 fn test_int32_to_bytes() {
-    let mut map = TestMapCarllerche::new();
+    let mut map = TestMapTokio::new();
     map.int32_to_string.insert(17, Chars::from("abc"));
     test_serialize_deserialize_with_dynamic("12 07 08 11 12 03 61 62 63", &map);
-    let mut map = TestMapCarllerche::new();
+    let mut map = TestMapTokio::new();
     map.int32_to_bytes.insert(17, Bytes::from("abc"));
     test_serialize_deserialize_with_dynamic("1a 07 08 11 12 03 61 62 63", &map);
 }
