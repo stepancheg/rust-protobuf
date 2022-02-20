c024251a956eb82a1d76d4ac9d8d96c79b09964e
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-20 00:16:49 +0000

    Fix tests with bytes feature enabled

--- protobuf-test/src/v2/test_tokio_bytes_default_value.rs
+++ protobuf-test/src/v2/test_tokio_bytes_default_value.rs
@@ -4,14 +4,8 @@ use super::test_tokio_bytes_default_value_pb::*;
 
 #[test]
 fn test_default_values() {
-    assert_eq!(
-        "sss",
-        TestTokioBytesDefaultValues::default_instance().get_s()
-    );
-    assert_eq!(
-        b"bbb",
-        TestTokioBytesDefaultValues::default_instance().get_b()
-    );
+    assert_eq!("sss", TestTokioBytesDefaultValues::default_instance().s());
+    assert_eq!(b"bbb", TestTokioBytesDefaultValues::default_instance().b());
     assert_eq!(&""[..], &**TestTokioBytesDefaultValues::new().mut_s());
     assert_eq!(&b""[..], &**TestTokioBytesDefaultValues::new().mut_b());
 }
