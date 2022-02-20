739e187f65b34dc5a69310bb4302445eaa432fa6
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-20 05:18:40 +0000

    Regenerate protos

--- protobuf/src/doctest_pb.rs
+++ protobuf/src/doctest_pb.rs
@@ -73,13 +73,13 @@ impl crate::Message for MyMessage {
     #[allow(unused_variables)]
     fn compute_size(&self) -> u64 {
         let mut my_size = 0;
-        my_size += crate::rt::unknown_fields_size(self.get_unknown_fields());
+        my_size += crate::rt::unknown_fields_size(self.unknown_fields());
         self.cached_size.set(my_size as u32);
         my_size
     }
 
     fn write_to_with_cached_sizes(&self, os: &mut crate::CodedOutputStream<'_>) -> crate::Result<()> {
-        os.write_unknown_fields(self.get_unknown_fields())?;
+        os.write_unknown_fields(self.unknown_fields())?;
         ::std::result::Result::Ok(())
     }
 
