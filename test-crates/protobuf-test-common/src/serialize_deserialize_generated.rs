500f77bad6f28495e4b897b9e444a70a48f61d90
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-21 02:53:37 +0000

    Fewer constraints in tests

--- test-crates/protobuf-test-common/src/serialize_deserialize_generated.rs
+++ test-crates/protobuf-test-common/src/serialize_deserialize_generated.rs
@@ -1,9 +1,11 @@
+use std::fmt;
+
 use protobuf::*;
 
 use crate::hex::decode_hex;
 use crate::hex::encode_hex;
 
-pub fn test_serialize_deserialize_length_delimited<M: MessageFull + PartialEq>(msg: &M) {
+pub fn test_serialize_deserialize_length_delimited<M: Message + PartialEq + fmt::Debug>(msg: &M) {
     let serialized_bytes = msg.write_length_delimited_to_bytes().unwrap();
     let mut is = CodedInputStream::from_bytes(&serialized_bytes);
     let parsed = is.read_message().unwrap();
@@ -17,17 +19,12 @@ pub fn test_serialize_deserialize_no_hex<M: MessageFull + PartialEq>(msg: &M) {
     assert_eq!(*msg, parsed);
 }
 
-pub fn test_serialize_deserialize<M: MessageFull + PartialEq>(hex: &str, msg: &M) {
+pub fn test_serialize_deserialize<M: Message + PartialEq + fmt::Debug>(hex: &str, msg: &M) {
     let expected_bytes = decode_hex(hex);
     let expected_hex = encode_hex(&expected_bytes);
     let serialized = msg.write_to_bytes().unwrap();
     let serialized_hex = encode_hex(&serialized);
-    assert_eq!(
-        expected_hex,
-        serialized_hex,
-        "message {}",
-        M::descriptor_static().name()
-    );
+    assert_eq!(expected_hex, serialized_hex, "message {}", M::NAME);
     let parsed = M::parse_from_bytes(&expected_bytes).unwrap();
     assert_eq!(*msg, parsed);
 
