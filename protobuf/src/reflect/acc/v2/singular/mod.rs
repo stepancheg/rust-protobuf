91a55fa45660e420d7fc27b31f607481999348ca
Stepan Koltsov <stepan.koltsov@gmail.com>
2022-04-04 07:00:34 +0100

    Simplify accessors

--- protobuf/src/reflect/acc/v2/singular/mod.rs
+++ protobuf/src/reflect/acc/v2/singular/mod.rs
@@ -154,42 +154,17 @@ impl SingularFieldAccessorHolder {
         M: MessageFull,
         V: MessageFull,
     {
-        struct Impl<M, V> {
-            get_field: for<'a> fn(&'a M) -> &'a MessageField<V>,
-            mut_field: for<'a> fn(&'a mut M) -> &'a mut MessageField<V>,
-        }
-
-        impl<M, V> SingularFieldAccessor for Impl<M, V>
-        where
-            M: MessageFull,
-            V: MessageFull,
-        {
-            fn get_field<'a>(&self, m: &'a dyn MessageDyn) -> Option<ReflectValueRef<'a>> {
-                let m = m.downcast_ref().unwrap();
-                (self.get_field)(m).as_option_ref().map(V::as_ref)
-            }
-
-            fn mut_field_or_default<'a>(&self, m: &'a mut dyn MessageDyn) -> ReflectValueMut<'a> {
-                let m = m.downcast_mut().unwrap();
-                let option = (self.mut_field)(m);
+        Self::new(
+            move |m| (get_field)(m).as_option_ref().map(V::as_ref),
+            move |m| {
+                let option = (mut_field)(m);
                 if option.as_option_ref().is_none() {
                     option.set_value(V::default());
                 }
                 V::as_mut(option.as_option_mut().unwrap())
-            }
-
-            fn set_field(&self, m: &mut dyn MessageDyn, value: ReflectValueBox) {
-                let m = m.downcast_mut().unwrap();
-                (self.mut_field)(m).set_value(V::from_value_box(value).expect("wrong type"));
-            }
-        }
-
-        SingularFieldAccessorHolder {
-            accessor: Box::new(Impl {
-                get_field,
-                mut_field,
-            }),
-        }
+            },
+            move |m, value| (mut_field)(m).set_value(V::from_value_box(value).expect("wrong type")),
+        )
     }
 
     pub(crate) fn new_get_option_set_enum<M, E>(
