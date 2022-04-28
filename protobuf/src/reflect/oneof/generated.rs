563d338c77275c620ccd5bdff17a942855e46d9e
Stepan Koltsov <stepan.koltsov@gmail.com>
2022-04-28 19:19:19 +0100

    Dead code

--- protobuf/src/reflect/oneof/generated.rs
+++ protobuf/src/reflect/oneof/generated.rs
@@ -6,20 +6,7 @@ pub struct GeneratedOneofDescriptorData {}
 
 impl GeneratedOneofDescriptorData {
     #[doc(hidden)]
-    pub fn new<O>(
-        _name_in_message: &'static str,
-    ) -> GeneratedOneofDescriptorData
-    where
-        O: OneofFull,
-    {
-        GeneratedOneofDescriptorData {}
-    }
-
-    #[doc(hidden)]
-    pub fn new_2<O>(
-        _name_in_file: &'static str,
-        _index_in_file: usize,
-    ) -> GeneratedOneofDescriptorData
+    pub fn new<O>(_name_in_message: &'static str) -> GeneratedOneofDescriptorData
     where
         O: OneofFull,
     {
