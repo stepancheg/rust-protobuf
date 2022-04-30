7804f6a96462db487cc829eb09c8a519e3f6b21b
Stepan Koltsov <stepan.koltsov@gmail.com>
2022-04-30 22:20:45 +0100

    Hide functions from public API

--- protobuf/src/rt/packed.rs
+++ protobuf/src/rt/packed.rs
@@ -7,21 +7,21 @@ use crate::Enum;
 use crate::EnumOrUnknown;
 
 /// Size of serialized repeated packed field, excluding length and tag.
-pub fn vec_packed_varint_data_size<T: ProtobufVarint>(vec: &[T]) -> u64 {
+pub(crate) fn vec_packed_varint_data_size<T: ProtobufVarint>(vec: &[T]) -> u64 {
     vec.iter()
         .map(|v| v.len_varint() as u64)
         .fold(0, |a, i| a + i)
 }
 
 /// Size of serialized repeated packed field, excluding length and tag.
-pub fn vec_packed_varint_zigzag_data_size<T: ProtobufVarintZigzag>(vec: &[T]) -> u64 {
+pub(crate) fn vec_packed_varint_zigzag_data_size<T: ProtobufVarintZigzag>(vec: &[T]) -> u64 {
     vec.iter()
         .map(|v| v.len_varint_zigzag())
         .fold(0, |a, i| a + i)
 }
 
 /// Size of serialized repeated packed enum field, excluding length and tag.
-pub fn vec_packed_enum_or_unknown_data_size<E: Enum>(vec: &[EnumOrUnknown<E>]) -> u64 {
+pub(crate) fn vec_packed_enum_or_unknown_data_size<E: Enum>(vec: &[EnumOrUnknown<E>]) -> u64 {
     vec.iter()
         .map(|e| compute_raw_varint32_size(e.value() as u32))
         .fold(0, |a, i| a + i)
