94eb46ae3741e62224358faa5849ebfe8ac10439
Stepan Koltsov <stepan.koltsov@gmail.com>
2022-04-30 21:59:49 +0100

    Revert "Code cleanup"
    This reverts commit 201630e712fabc090506ceee7ef10d40f6b1031a.

--- protobuf/src/rt/mod.rs
+++ protobuf/src/rt/mod.rs
@@ -132,19 +132,23 @@ impl ProtobufFixed for bool {
 
 /// Size of serialized repeated packed field, excluding length and tag.
 pub fn vec_packed_varint_data_size<T: ProtobufVarint>(vec: &[T]) -> u64 {
-    vec.iter().map(|v| v.len_varint() as u64).sum()
+    vec.iter()
+        .map(|v| v.len_varint() as u64)
+        .fold(0, |a, i| a + i)
 }
 
 /// Size of serialized repeated packed field, excluding length and tag.
 pub fn vec_packed_varint_zigzag_data_size<T: ProtobufVarintZigzag>(vec: &[T]) -> u64 {
-    vec.iter().map(|v| v.len_varint_zigzag()).sum()
+    vec.iter()
+        .map(|v| v.len_varint_zigzag())
+        .fold(0, |a, i| a + i)
 }
 
 /// Size of serialized repeated packed enum field, excluding length and tag.
 pub fn vec_packed_enum_or_unknown_data_size<E: Enum>(vec: &[EnumOrUnknown<E>]) -> u64 {
     vec.iter()
         .map(|e| compute_raw_varint32_size(e.value() as u32))
-        .sum()
+        .fold(0, |a, i| a + i)
 }
 
 /// Size of serialized data with length prefix and tag
