b44061cfe8539277675f1557b49ffcc7e6fd2c12
Stepan Koltsov <stepan.koltsov@gmail.com>
2022-05-02 08:26:00 +0100

    Up to date readme

--- protobuf/README.md
+++ protobuf/README.md
@@ -2,19 +2,6 @@
 
 # Library to read and write protocol buffers data
 
-## Version 3 is alpha
-
-Currently developed branch of rust-protobuf is 3. It has the same spirit as version 2,
-but contains numerous improvements like:
-* runtime reflection for mutability, not just for access
-* protobuf text format and JSON parsing (which rely on reflection)
-* dynamic message support: work with protobuf data without generating code from schema
-* lite runtime codegen option now produces correct code without reflection support
-
-Stable version of rust-protobuf will be supported until version 3 released.
-
-[Tracking issue for version 3](https://github.com/stepancheg/rust-protobuf/issues/518).
-
 ## Features
 
 This crate has one feature, which is `with-bytes`.
@@ -28,10 +15,10 @@ Note, codegen also need to be instructed to generate `Bytes` or `Chars` for
 `bytes` or `string` protobuf types instead of default `Vec<u8>` or `String`,
 just enabling option on this crate is not enough.
 
-See `Customize` struct in [`protobuf-codegen` crate](https://docs.rs/protobuf-codegen/%3E=3.0.0-alpha).
+See `Customize` struct in [`protobuf-codegen` crate](https://docs.rs/protobuf-codegen).
 
 ## Accompanying crates
 
-* [`protobuf-json-mapping`](https://docs.rs/protobuf-json-mapping/%3E=3.0.0-alpha)
+* [`protobuf-json-mapping`](https://docs.rs/protobuf-json-mapping)
 
 <!-- cargo-sync-readme end -->
--- protobuf/src/lib.rs
+++ protobuf/src/lib.rs
@@ -1,18 +1,5 @@
 //! # Library to read and write protocol buffers data
 //!
-//! ## Version 3 is alpha
-//!
-//! Currently developed branch of rust-protobuf is 3. It has the same spirit as version 2,
-//! but contains numerous improvements like:
-//! * runtime reflection for mutability, not just for access
-//! * protobuf text format and JSON parsing (which rely on reflection)
-//! * dynamic message support: work with protobuf data without generating code from schema
-//! * lite runtime codegen option now produces correct code without reflection support
-//!
-//! Stable version of rust-protobuf will be supported until version 3 released.
-//!
-//! [Tracking issue for version 3](https://github.com/stepancheg/rust-protobuf/issues/518).
-//!
 //! ## Features
 //!
 //! This crate has one feature, which is `with-bytes`.
@@ -26,17 +13,17 @@
 //! `bytes` or `string` protobuf types instead of default `Vec<u8>` or `String`,
 //! just enabling option on this crate is not enough.
 //!
-//! See `Customize` struct in [`protobuf-codegen` crate](https://docs.rs/protobuf-codegen/%3E=3.0.0-alpha).
+//! See `Customize` struct in [`protobuf-codegen` crate](https://docs.rs/protobuf-codegen).
 //!
 //! ## Accompanying crates
 //!
-//! * [`protobuf-json-mapping`](https://docs.rs/protobuf-json-mapping/%3E=3.0.0-alpha)
+//! * [`protobuf-json-mapping`](https://docs.rs/protobuf-json-mapping)
 //    implements JSON parsing and serialization for protobuf messages.
-//! * [`protobuf-codegen`](https://docs.rs/protobuf-codegen/%3E=3.0.0-alpha)
+//! * [`protobuf-codegen`](https://docs.rs/protobuf-codegen)
 //!   can be used to rust code from `.proto` crates.
-//! * [`protoc-bin-vendored`](https://docs.rs/protoc-bin-vendored/%3E=3.0.0-alpha)
+//! * [`protoc-bin-vendored`](https://docs.rs/protoc-bin-vendored)
 //!   contains `protoc` command packed into the crate.
-//! * [`protobuf-parse`](https://docs.rs/protobuf-parse/%3E=3.0.0-alpha) contains
+//! * [`protobuf-parse`](https://docs.rs/protobuf-parse) contains
 //!   `.proto` file parser. Rarely need to be used directly,
 //!   but can be used for mechanical processing of `.proto` files.
 
