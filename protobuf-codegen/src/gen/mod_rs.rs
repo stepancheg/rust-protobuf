7131fb244fb1246d2835f5ad7426e607ee7c4a1f
hanabi1224 <harlowmoo@gmail.com>
2022-05-06 13:41:42 +0800

    Fix generated mod.rs to pass cargo fmt -- --check

--- protobuf-codegen/src/gen/mod_rs.rs
+++ protobuf-codegen/src/gen/mod_rs.rs
@@ -5,6 +5,8 @@ pub(crate) fn gen_mod_rs(mods: &[String]) -> compiler_plugin::GenResult {
     let v = CodeWriter::with_no_error(|w| {
         w.comment(&format!("{}generated", "@"));
         w.write_line("");
+        let mut mods: Vec<&String> = mods.into_iter().collect();
+        mods.sort();
         for m in mods {
             w.write_line(&format!("pub mod {};", m));
         }
