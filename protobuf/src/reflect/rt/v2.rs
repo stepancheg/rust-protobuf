d39fec50eb3605941b9bff6caaa64e44dd1dc452
Stepan Koltsov <stepan.koltsov@gmail.com>
2024-06-26 00:57:02 +0100

    Sync map accessors implementation from PR #700

--- protobuf/src/reflect/rt/v2.rs
+++ protobuf/src/reflect/rt/v2.rs
@@ -3,6 +3,7 @@
 #![doc(hidden)]
 
 pub use crate::reflect::acc::v2::map::make_map_simpler_accessor;
+pub use crate::reflect::acc::v2::map::make_map_simpler_accessor_new;
 pub use crate::reflect::acc::v2::repeated::make_vec_simpler_accessor;
 pub use crate::reflect::acc::v2::singular::make_message_field_accessor;
 pub use crate::reflect::acc::v2::singular::make_option_accessor;
